9e6d180d621d4f1154cf616fcfde4ee96dae3028
Dave <Daverevell491@gmail.com>
2022-11-04 15:22:42 +0000

    Click event completed

--- src/App.js
+++ src/App.js
@@ -6,12 +6,42 @@ function App() {
   const [operator, setOperator] = useState("+");
   const [number2, setNumber2] = useState(0);
   const [result, calculateResult] = useState(0);
-  const assignNumber1 = () => setNumber1;
+
+  const assignNumber1 = (event) => {
+    if (event.target.innerText != 'Clear') {
+      setNumber1(event.target.innerText)
+    } else {
+      setNumber1(0)
+    }
+  }
+
+  const assignOperator = (event) => {
+    
+    setOperator(event.target.innerText)
+    
+    
+    console.log(operator)
+  }
+
+  const assignNumber2 = (event) => {
+    if (event.target.innerText != 'Clear') {
+      setNumber2(event.target.innerText)
+    } else {
+      setNumber2(0)
+    }
+    console.log(number2)
+  }
+
+  const assignResult = (event) => {
+    let result = calculateResult(number1 + operator + number2)
+    result = parseInt(result)
+    console.log(result)
+  }
   return (
     <div className="calculator">
       <div className="panel">
         <p>{number1}</p>
-        <div className="numbers">
+        <div className="numbers" onClick={assignNumber1}>
           <button>1</button>
           <button>2</button>
           <button>3</button>
@@ -28,7 +58,7 @@ function App() {
 
       <div className="panel">
         <p>{operator}</p>
-        <div className="numbers">
+        <div className="numbers" onClick={assignOperator}>
           <button>+</button>
           <button>-</button>
           <button>*</button>
@@ -38,7 +68,7 @@ function App() {
 
       <div className="panel">
         <p>{number2}</p>
-        <div className="numbers">
+        <div className="numbers" onClick={assignNumber2}>
           <button>1</button>
           <button>2</button>
           <button>3</button>
@@ -54,7 +84,7 @@ function App() {
       </div>
       <div className="panel answer">
         <p>{result}</p>
-        <div>
+        <div onClick={assignResult}>
           <button>=</button>
         </div>
       </div>
