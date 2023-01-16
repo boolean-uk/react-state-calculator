2f5818c98f2ebdab6477e0beb323558563afc231
benji <benjipictor@gmail.com>
2023-01-16 15:30:11 +0000

    core and ext2 complete

--- src/App.js
+++ src/App.js
@@ -1,58 +1,121 @@
 import "./App.css"
+import { useState } from "react"
 
-function App() { 
+function App() {
+  // const [total, action] = useState(0)
+  const [num1, updateNum1] = useState('0')
+  const [num2, updateNum2] = useState('0')
+  const [operator, updateOperator] = useState('+')
+  const [total, updateTotal] = useState(0)
+
+  function handleNum1(event) {
+    if (num1 === '0') {
+      updateNum1(event.target.innerText)
+      return
+    }
+    else {
+      updateNum1(num1 + event.target.innerText)
+      // console.log(num1)
+    }
+  }
+  function handleNum2(event) {
+    if(num2 === '0'){
+      updateNum2(event.target.innerText)
+    }
+    else{
+      updateNum2(num2 + event.target.innerText)
+    }
+  }
+  function handleOperator(event) {
+
+    updateOperator(event.target.innerText)
+  }
+  function clearNum1() {
+    updateNum1(0)
+  }
+  function clearNum2() {
+    updateNum2(0)
+  }
+
+  function calculate() {
+    // let subtotal = 0
+    if (operator === '+') {
+      updateTotal(Number(num1) + Number(num2))
+      clearNum1()
+      clearNum2()
+      return
+    }
+    if (operator === '-') {
+      updateTotal(Number(num1) - Number(num2))
+      clearNum1()
+      clearNum2()
+      return
+    }
+    if (operator === '*') {
+      updateTotal(Number(num1) * Number(num2))
+      clearNum1()
+      clearNum2()
+      return
+    }
+    if (operator === '÷') {
+      updateTotal(Number(num1) / Number(num2))
+      clearNum1()
+      clearNum2()
+      return
+    }
+  }
 
   return (
     <div className="calculator">
-        <div className="panel">
-          <p>0</p>
-          <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
-          </div>
+      <div className="panel">
+        <p>{num1}</p>
+        <div className="numbers">
+          <button onClick={handleNum1}>1</button>
+          <button onClick={handleNum1}>2</button>
+          <button onClick={handleNum1}>3</button>
+          <button onClick={handleNum1}>4</button>
+          <button onClick={handleNum1}>5</button>
+          <button onClick={handleNum1}>6</button>
+          <button onClick={handleNum1}>7</button>
+          <button onClick={handleNum1}>8</button>
+          <button onClick={handleNum1}>9</button>
+          <button onClick={handleNum1}>0</button>
+          <button onClick={clearNum1}>Clear</button>
         </div>
-        
-        <div className="panel">
-          <p>+</p>
-          <div className="numbers">
-            <button>+</button>
-            <button>-</button>
-            <button>*</button>
-            <button>÷</button>
-          </div>
-        </div>
+      </div>
 
-        <div className="panel">
-          <p>0</p>
-          <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
-          </div>
+      <div className="panel">
+        <p>{operator}</p>
+        <div className="numbers">
+          <button onClick={handleOperator}>+</button>
+          <button onClick={handleOperator}>-</button>
+          <button onClick={handleOperator}>*</button>
+          <button onClick={handleOperator}>÷</button>
         </div>
-        <div className="panel answer">
-          <p>0</p>
-          <div>
-            <button>=</button>
-          </div>
+      </div>
+
+      <div className="panel">
+        <p>{num2}</p>
+        <div className="numbers">
+          <button onClick={handleNum2}>1</button>
+          <button onClick={handleNum2}>3</button>
+          <button onClick={handleNum2}>2</button>
+          <button onClick={handleNum2}>4</button>
+          <button onClick={handleNum2}>5</button>
+          <button onClick={handleNum2}>6</button>
+          <button onClick={handleNum2}>7</button>
+          <button onClick={handleNum2}>8</button>
+          <button onClick={handleNum2}>9</button>
+          <button onClick={handleNum2}>0</button>
+          <button onClick={clearNum2}>Clear</button>
         </div>
+      </div>
+      <div className="panel answer">
+        <p>{total}</p>
+        <div>
+          <button onClick={calculate}>=</button>
+        </div>
+      </div>
     </div>
   )
 }
