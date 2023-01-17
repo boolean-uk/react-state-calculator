cd8be6ec7343dac8253d2cd0c3fdacfe33db4d52
Kat <katdemey@outlook.com>
2023-01-17 10:36:34 +0000

    extension one 90% working. Can append numbers to each other appropriately, but the + function is appending num1 and num2 instead of adding them as a sum

--- src/App.js
+++ src/App.js
@@ -1,79 +1,102 @@
 import "./App.css"
 import { useState } from "react"
 
-function App() { 
+function App() {
 
-  const [numOne,setNumOne]=useState(0)
-  const [operator,setOperator]=useState("+")
-  const [numTWo,setNumTwo]=useState(0)
-  const [answer,setAnswer]=useState(0)
+  const [numOne, setNumOne] = useState(0)
+  const [operator, setOperator] = useState("+")
+  const [numTwo, setNumTwo] = useState(0)
+  const [answer, setAnswer] = useState(0)
 
-//calculation
-function calculate(){
- if(operator === "+"){
-  setAnswer(numOne+numTWo)
+ const handleNumOne = (click) => {
+  const inputNumber = click.target.innerText
+
+  if (numOne === 0) {
+    setNumOne(inputNumber)
+  } else {
+    setNumOne(numOne + inputNumber)
+  }
  }
- else if(operator === "-"){
-  setAnswer(numOne - numTWo)
- }else if(operator === "*"){
-  setAnswer(numOne * numTWo)
+
+ const handleNumTwo = (click) => {
+  const inputNumber = click.target.innerText
+
+  if (numTwo === 0) {
+    setNumTwo(inputNumber)
+  } else {
+    setNumTwo(numTwo + inputNumber)
+  }
  }
- else if(operator === "÷"){
-  setAnswer(numOne / numTWo)
- }
-}
+
+  //calculation
+  function calculate() {
+    JSON.parse(numOne)
+    JSON.parse(numTwo)
+
+    if (operator === "+") {
+      setAnswer( numOne + numTwo)
+    }
+    else if (operator === "-") {
+      setAnswer(numOne - numTwo)
+    } else if (operator === "*") {
+      setAnswer(numOne * numTwo)
+    }
+    else if (operator === "÷") {
+      setAnswer(numOne / numTwo)
+    }
+  }
 
   return (
     <div className="calculator">
-        <div className="panel">
-          <p>{numOne}</p>
-          <div className="numbers">
-            <button onClick={()=> setNumOne(1)}>1</button>
-            <button onClick={()=> setNumOne(2)}>2</button>
-            <button onClick={()=> setNumOne(3)}>3</button>
-            <button onClick={()=> setNumOne(4)}>4</button>
-            <button onClick={()=> setNumOne(5)}>5</button>
-            <button onClick={()=> setNumOne(6)}>6</button>
-            <button onClick={()=> setNumOne(7)}>7</button>
-            <button onClick={()=> setNumOne(8)}>8</button>
-            <button onClick={()=> setNumOne(9)}>9</button>
-            <button onClick={()=> setNumOne(0)}>0</button>
-            <button onClick={()=> setNumOne(0)}>Clear</button>
-          </div>
+      <div className="panel">
+        <p>{numOne}</p>
+        <div className="numbers">
+          <button onClick={handleNumOne}>1</button>
+          <button onClick={handleNumOne}>2</button>
+          <button onClick={handleNumOne}>3</button>
+          <button onClick={handleNumOne}>4</button>
+          <button onClick={handleNumOne}>5</button>
+          <button onClick={handleNumOne}>6</button>
+          <button onClick={handleNumOne}>7</button>
+          <button onClick={handleNumOne}>8</button>
+          <button onClick={handleNumOne}>9</button>
+          <button onClick={handleNumOne}>0</button>
+          <button onClick={() => setNumOne(0)}>Clear</button>
         </div>
-        
-        <div className="panel">
-          <p>{operator}</p>
-          <div className="numbers">
-            <button  onClick={() =>setOperator("+")}>+</button>
-            <button onClick={() =>setOperator("-")}>-</button>
-            <button onClick={() =>setOperator("*")}>*</button>
-            <button onClick={() =>setOperator("÷")}>÷</button>
-          </div>
-        </div>
+      </div>
 
-        <div className="panel">
-          <p>{numTWo}</p>
-          <div className="numbers">
-          <button onClick={()=> setNumTwo(1)}>1</button>
-            <button onClick={()=> setNumTwo(2)}>2</button>
-            <button onClick={()=> setNumTwo(3)}>3</button>
-            <button onClick={()=> setNumTwo(4)}>4</button>
-            <button onClick={()=> setNumTwo(5)}>5</button>
-            <button onClick={()=> setNumTwo(6)}>6</button>
-            <button onClick={()=> setNumTwo(7)}>7</button>
-            <button onClick={()=> setNumTwo(8)}>8</button>
-            <button onClick={()=> setNumTwo(9)}>9</button>
-            <button onClick={()=> setNumTwo(0)}>0</button>
-            <button onClick={()=> setNumTwo(0)}>Clear</button>
-          </div>
+      <div className="panel">
+        <p>{operator}</p>
+        <div className="numbers">
+          <button onClick={() => setOperator("+")}>+</button>
+          <button onClick={() => setOperator("-")}>-</button>
+          <button onClick={() => setOperator("*")}>*</button>
+          <button onClick={() => setOperator("÷")}>÷</button>
         </div>
-        <div className="panel answer">
-          <p>{answer}</p>
-          <div>
-            <button onClick={() => calculate()}>=</button>
-          </div>
+      </div>
+
+      <div className="panel">
+        <p>{numTwo}</p>
+        <div className="numbers">
+          <button onClick={handleNumTwo}>1</button>
+          <button onClick={handleNumTwo}>2</button>
+          <button onClick={handleNumTwo}>3</button>
+          <button onClick={handleNumTwo}>4</button>
+          <button onClick={handleNumTwo}>5</button>
+          <button onClick={handleNumTwo}>6</button>
+          <button onClick={handleNumTwo}>7</button>
+          <button onClick={handleNumTwo}>8</button>
+          <button onClick={handleNumTwo}>9</button>
+          <button onClick={handleNumTwo}>0</button>
+          <button onClick={() => setNumTwo(0)}>Clear</button>
         </div>
+      </div>
+      <div className="panel answer">
+        <p>{answer}</p>
+        <div>
+          <button onClick={() => calculate()}>=</button>
+        </div>
+      </div>
     </div>
   )
 }
