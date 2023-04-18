ac6358a5e42d773a89b87187c6b92eb2dd1149c3
PanagiotisTziolos <panagiotistziolos@outlook.com>
2023-04-18 18:39:52 +0300

    completed extension 1

--- src/App.js
+++ src/App.js
@@ -5,13 +5,43 @@ import Button from './Button'
 import Operators from './Operators'
 
 function App() { 
-  const [firstNumber, setFirstP] = useState(0)
+  const [firstNumber, setFirstNumber] = useState('0')
   // to display sympols (+, *) make them 'strings'
   // if its not a number or a boolean make it a string! 
-  const [operator, setFirstO] = useState('+')
+  const [operator, setOperator] = useState('+')
   const [secondNumber, setSecondNumber] = useState(0)
   const [result, setResult] = useState(0)
 
+  // We use string instead of numbers, because we
+  // can concatenate strings
+  // e.g. '1' + '2' = '12'
+  function fun(value) {
+    if (firstNumber.length <= 8) {
+      // if number is 0, overwrite it
+      // with the value
+      if (firstNumber === '0')
+        setFirstNumber(value)
+      // if the number is not equal to 0
+      // concatenate with the value
+      else
+        setFirstNumber(firstNumber + value)
+    }
+  }
+
+  function fun2(value) {
+    if (secondNumber.length <= 8) {
+      // if number is 0, overwrite it
+      // with the value
+      if (secondNumber === '0') {
+        setSecondNumber(value)
+      }
+      // if the number is not equal to 0
+      // concatenate with the value
+      else {
+        setSecondNumber(secondNumber + value)
+      }
+    }
+  }
 
   return (
     <div className="calculator">
@@ -26,47 +56,52 @@ function App() {
                   Button component and assign it to the onClick function
                   So, when the button is clicked, the firstP function is called
             */}
-            <Button value={1} fun={setFirstP} />
-            <Button value={2} fun={setFirstP} />
-            <Button value={3} fun={setFirstP} />
-            <Button value={4} fun={setFirstP} />
-            <Button value={5} fun={setFirstP} />
-            <Button value={6} fun={setFirstP} />
-            <Button value={7} fun={setFirstP} />
-            <Button value={8} fun={setFirstP} />
-            <Button value={9} fun={setFirstP} />
-            <Button value={0} fun={setFirstP} />
-            <Button value={'Clear'} fun={setFirstP} />
+            <Button value={'1'} fun={fun} />
+            <Button value={'2'} fun={fun} />
+            <Button value={'3'} fun={fun} />
+            <Button value={'4'} fun={fun} />
+            <Button value={'5'} fun={fun} />
+            <Button value={'6'} fun={fun} />
+            <Button value={'7'} fun={fun} />
+            <Button value={'8'} fun={fun} />
+            <Button value={'9'} fun={fun} />
+            <Button value={'0'} fun={fun} />
+            {/*Clear change the value to the string '0'*/}
+            <Button value={'Clear'} fun={(_) => setFirstNumber('0') } />
           </div>
         </div>
         
         <div className="panel">
           <p>{operator}</p>
           <div className="numbers">
-            <Operators value={'+'} fun={setFirstO} />
-            <Operators value={'-'} fun={setFirstO} />
-            <Operators value={'*'} fun={setFirstO} />
-            <Operators value={'÷'} fun={setFirstO} />
+            <Operators value={'+'} fun={setOperator} />
+            <Operators value={'-'} fun={setOperator} />
+            <Operators value={'*'} fun={setOperator} />
+            <Operators value={'÷'} fun={setOperator} />
           </div>
         </div>
 
         <div className="panel">
           <p>{secondNumber}</p>
           <div className="numbers">
-            <Button value={1} fun={setSecondNumber} />
-            <Button value={2} fun={setSecondNumber} />
-            <Button value={3} fun={setSecondNumber} />
-            <Button value={4} fun={setSecondNumber} />
-            <Button value={5} fun={setSecondNumber} />
-            <Button value={6} fun={setSecondNumber} />
-            <Button value={7} fun={setSecondNumber} />
-            <Button value={8} fun={setSecondNumber} />
-            <Button value={9} fun={setSecondNumber} />
-            <Button value={0} fun={setSecondNumber} />
-            {/* we have to make a buttun with the clear function*/}
-            <Button value={'Clear'} fun={setSecondNumber(0)} />
-              {/* function clear = 0 value of secondNumber*/}
-              {/*  clear turns the value of the secondNumber to zero */}
+          <Button value={'1'} fun={fun2} />
+            <Button value={'2'} fun={fun2} />
+            <Button value={'3'} fun={fun2} />
+            <Button value={'4'} fun={fun2} />
+            <Button value={'5'} fun={fun2} />
+            <Button value={'6'} fun={fun2} />
+            <Button value={'7'} fun={fun2} />
+            <Button value={'8'} fun={fun2} />
+            <Button value={'9'} fun={fun2} />
+            <Button value={'0'} fun={fun2} />
+            {/*Clear change the value to the string '0' 
+              we have to make a buttun with the clear function
+              function clear = 0 value of secondNumber
+              clear turns the value of the secondNumber to zero
+              we pass as a parameter a function that takes an argument,
+              but it doesn't use it
+            */}
+            <Button value={'Clear'} fun={(_) => setSecondNumber('0') } />
           </div>
         </div>
         <div className="panel answer">
@@ -74,17 +109,25 @@ function App() {
           <div>
             <Button value={'='} fun={function() {
               let answer = 0
+              {/*
+                To convert a string to a number:
+                  1) use the Number object
+                  2) use the + or - sign at the beginning of the string
+                  3) Number.parseInt('string')
+                  4) Number.parseFloat('string')
+                */}
               if (operator === '+')
-                answer = firstNumber + secondNumber
+                answer = Number(firstNumber) + Number(secondNumber)
               else if (operator === '-')
-                answer = firstNumber - secondNumber
+                answer = +firstNumber - +secondNumber
               else if (operator === '*')
-                answer = firstNumber * secondNumber
+                answer = +firstNumber * +secondNumber
               else if (operator === '÷')
-                answer = firstNumber / secondNumber
-              
-              {/*call the setResult function and pass the answer*/}
-              {/* as a parameter to change the value of the result*/}
+                answer = +firstNumber / +secondNumber
+              {/*
+                call the setResult function and pass the answer
+                as a parameter to change the value of the result
+              */}
               setResult(answer)
             }} />
           </div>
--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,7 @@
 
 // value is used as the number of the button e.g 1, 2, 3, 4
 // setButtonValue is a user defined 
-export default function Button({ value, fun }) {
+export default function Button({ value, fun}) {
 
     const setButtonValue = () => { fun(value) }
 
