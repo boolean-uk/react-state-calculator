4827a1a83bc41e2d147b64cce6468d6acf1dffd1
Stavros <spiliopoulos.stavros@hotmail.com>
2023-04-18 16:48:55 +0300

    Standard completed

--- src/App.js
+++ src/App.js
@@ -1,56 +1,110 @@
 import "./App.css"
+import { useState } from 'react'
 
 function App() { 
+
+const [num1, setNum1] = useState(0)
+const [num2, setNum2] = useState(0)
+const [symbol, setSymbol] = useState('+')
+const [result, setResult] = useState(0) // TODO: remove
+
+const handlerNum1 = (event) => {
+  const value = event.target.innerText
+  if (num1 == 0) {
+    setNum1(value)
+  } else {
+    setNum1(num1 + value)
+  } 
+}
+
+const handlerNum2 = (event) => {
+  const value = event.target.innerText
+  if (num2 == 0){
+    setNum2(value)
+  } else {
+    setNum2(num2 + value)
+  }
+}
+
+const handlerClearNum1 = () => {
+  setNum1(0)
+}
+
+const handlerClearNum2 = () => {
+  setNum2(0)
+}
+
+const handlerSymbol = (event) => {
+  const value = event.target.innerText
+  setSymbol(value)
+}
+
+const calculate = () => {
+  const int1 = parseInt(num1)
+  const int2 = parseInt(num2)
+  let result = 0
+  if (symbol === '+') {
+    result = int1 + int2
+  } else if (symbol === '-') {
+    result = int1 - int2
+  } else if (symbol === '*') {
+    result = int1 * int2
+  } else if (symbol === '÷') {
+    result = int1 / int2
+  }
+  setResult(result)
+}
+
 
   return (
     <div className="calculator">
         <div className="panel">
-          <p>0</p>
+          <p>{num1}</p>
           <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
+            <button onClick={handlerNum1}>1</button>
+            <button onClick={handlerNum1}>2</button>
+            <button onClick={handlerNum1}>3</button>
+            <button onClick={handlerNum1}>4</button>
+            <button onClick={handlerNum1}>5</button>
+            <button onClick={handlerNum1}>6</button>
+            <button onClick={handlerNum1}>7</button>
+            <button onClick={handlerNum1}>8</button>
+            <button onClick={handlerNum1}>9</button>
+            <button onClick={handlerNum1}>0</button>
+            <button onClick={handlerClearNum1}>Clear</button>
           </div>
         </div>
         
         <div className="panel">
-          <p>+</p>
+          <p>{symbol}</p>
           <div className="numbers">
-            <button>+</button>
-            <button>-</button>
-            <button>*</button>
-            <button>÷</button>
+            <button onClick={handlerSymbol}>+</button>
+            <button onClick={handlerSymbol}>-</button>
+            <button onClick={handlerSymbol}>*</button>
+            <button onClick={handlerSymbol}>÷</button>
           </div>
         </div>
 
         <div className="panel">
-          <p>0</p>
+          <p>{num2}</p>
           <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
+            <button onClick={handlerNum2}>1</button>
+            <button onClick={handlerNum2}>2</button>
+            <button onClick={handlerNum2}>3</button>
+            <button onClick={handlerNum2}>4</button>
+            <button onClick={handlerNum2}>5</button>
+            <button onClick={handlerNum2}>6</button>
+            <button onClick={handlerNum2}>7</button>
+            <button onClick={handlerNum2}>8</button>
+            <button onClick={handlerNum2}>9</button>
+            <button onClick={handlerNum2}>0</button>
+            <button onClick={handlerClearNum2}>Clear</button>
           </div>
         </div>
         <div className="panel answer">
-          <p>0</p>
+          <p>{result}</p>
           <div>
-            <button>=</button>
+            <button onClick={calculate}>=</button>
           </div>
         </div>
     </div>
