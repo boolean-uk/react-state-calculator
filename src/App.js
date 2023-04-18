1cbfd44de0702938a0c8c70bf2174f3fac92b6bf
Carmen Barcia <carmen.barcia@hotmail.com>
2023-04-18 17:04:43 +0200

    finish the calculator

--- src/App.js
+++ src/App.js
@@ -3,22 +3,12 @@ import {useState} from 'react'
 
 
 function App() { 
-<<<<<<< HEAD
-const [number1, setNumber1] = useState(0) 
-//const [number2, setNumber2] = useState(0)
-
-//const [addcount , setAddCount ] = useState(0)
-
-
-
-const clickNumber1 = (event) => {
-  event.preventDefault()
-  console.log(event)
-const number11 = event.target[1]._reactProps$7fx6lcrc3dj.id
-console.log(number11)
-}
-=======
   const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+
+  const [operator, setOperator] = useState(0);
+  const [result, setResult] = useState(0);
+  //const [adding, setAdding] = useState(0);
 
   const handleNumberClick = (num) => {
     setNumber1((prevNumber) => {
@@ -31,8 +21,40 @@ console.log(number11)
     setNumber1(0);
   }; 
 
+  const handleNumber2Click = (num) => {
+    setNumber2((prevNumber) => {
+      const newNumber = prevNumber === 0 ? num : Number(`${prevNumber}${num}`);
+      return newNumber;
+    })
+  };
 
->>>>>>> 6d4f8c9a4c1d7b2c72ed4870e6b66b8aca062ffd
+  const handleClearClick2 = () => {
+    setNumber2(0);
+  }
+
+  const handleOperator = (op) => {
+    setOperator(op)
+  }
+
+  // const addingFunction = () => {
+  //   setAdding(number1 + number2) 
+
+  // }
+  const outputResult = () => {
+    let finalResult = 0
+    if (operator === '+') {
+        finalResult = number1 + number2
+      } else if (operator === '-') {
+        finalResult = number1 - number2
+      } else if (operator === '*') {
+        finalResult = number1*number2
+      } else if (operator === '÷') {
+        finalResult = number1/number2
+      }
+    setResult(finalResult)
+  }
+
+
   return (
     <div className="calculator">
         <div className="panel">
@@ -53,35 +75,35 @@ console.log(number11)
         </div>
         
         <div className="panel">
-          <p>+</p>
+          <p>{operator}</p>
           <div className="numbers">
-            <button>+</button>
-            <button>-</button>
-            <button>*</button>
-            <button>÷</button>
+            <button onClick={() => handleOperator('+')}>+</button>
+            <button onClick={() => handleOperator('-')}>-</button>
+            <button onClick={() => handleOperator('*')}>*</button>
+            <button onClick={() => handleOperator('÷')}>÷</button>
           </div>
         </div>
 
         <div className="panel">
-          <p>0</p>
+          <p>{number2}</p>
           <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
+            <button onClick={() => handleNumber2Click(1)}>1</button>
+            <button onClick={() => handleNumber2Click(2)}>2</button>
+            <button onClick={() => handleNumber2Click(3)}>3</button>
+            <button onClick={() => handleNumber2Click(4)}>4</button>
+            <button onClick={() => handleNumber2Click(5)}>5</button>
+            <button onClick={() => handleNumber2Click(6)}>6</button>
+            <button onClick={() => handleNumber2Click(7)}>7</button>
+            <button onClick={() => handleNumber2Click(8)}>8</button>
+            <button onClick={() => handleNumber2Click(9)}>9</button>
+            <button onClick={() => handleNumber2Click(0)}>0</button>
+            <button onClick={handleClearClick2}>Clear</button>
           </div>
         </div>
         <div className="panel answer">
-          <p>0</p>
+          <p>{result}</p>
           <div>
-            <button>=</button>
+            <button onClick={() => outputResult()}>=</button>
           </div>
         </div>
     </div>
