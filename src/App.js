94ccd3c5be84168b79b4a6a377283f92ab6087f8
Max de Ruiter <m.j.deruiter99@hotmail.com>
2023-04-18 14:55:16 +0200

    Standard exercize

--- src/App.js
+++ src/App.js
@@ -1,60 +1,108 @@
-import "./App.css"
+import { useState } from "react";
 
-function App() { 
+import "./App.css";
+
+function App() {
+  const [numberOne, setNumberOne] = useState(0);
+  const [symbol, setSymbol] = useState("+");
+  const [numberTwo, setNumberTwo] = useState(0);
+  const [result, setNumberResult] = useState(0);
+
+  const handleInputOne = (event) => {
+    const numberButton = event.target.innerText;
+    setNumberOne(numberButton);
+  };
+
+  const handleInputSymbol = (event) => {
+    const symbolButton = event.target.innerText;
+    setSymbol(symbolButton);
+  };
+
+  const handleInputTwo = (event) => {
+    const numberButton = event.target.innerText;
+    setNumberTwo(numberButton);
+  };
+
+  const clearOne = () => {
+    setNumberOne(0);
+  };
+
+  const clearTwo = () => {
+    setNumberTwo(0);
+  };
+
+  const handleInputResult = (event) => {
+    switch (symbol) {
+      case "+":
+        setNumberResult(Number(numberOne) + Number(numberTwo));
+        break;
+      case "-":
+        setNumberResult(Number(numberOne) - Number(numberTwo));
+        break;
+      case "*":
+        setNumberResult(Number(numberOne) * Number(numberTwo));
+        break;
+      case "÷":
+        setNumberResult(Number(numberOne) / Number(numberTwo));
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
     <div className="calculator">
-        <div className="panel">
-          <p>0</p>
-          <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
-          </div>
+      <div className="panel">
+        <p>{numberOne}</p>
+        <div className="numbers">
+          <button onClick={handleInputOne}>1</button>
+          <button onClick={handleInputOne}>2</button>
+          <button onClick={handleInputOne}>3</button>
+          <button onClick={handleInputOne}>4</button>
+          <button onClick={handleInputOne}>5</button>
+          <button onClick={handleInputOne}>6</button>
+          <button onClick={handleInputOne}>7</button>
+          <button onClick={handleInputOne}>8</button>
+          <button onClick={handleInputOne}>9</button>
+          <button onClick={handleInputOne}>0</button>
+          <button onClick={clearOne}>Clear</button>
         </div>
-        
-        <div className="panel">
-          <p>+</p>
-          <div className="numbers">
-            <button>+</button>
-            <button>-</button>
-            <button>*</button>
-            <button>÷</button>
-          </div>
-        </div>
+      </div>
 
-        <div className="panel">
-          <p>0</p>
-          <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
-          </div>
+      <div className="panel">
+        <p>{symbol}</p>
+        <div className="numbers">
+          <button onClick={handleInputSymbol}>+</button>
+          <button onClick={handleInputSymbol}>-</button>
+          <button onClick={handleInputSymbol}>*</button>
+          <button onClick={handleInputSymbol}>÷</button>
         </div>
-        <div className="panel answer">
-          <p>0</p>
-          <div>
-            <button>=</button>
-          </div>
+      </div>
+
+      <div className="panel">
+        <p>{numberTwo}</p>
+        <div className="numbers">
+          <button onClick={handleInputTwo}>1</button>
+          <button onClick={handleInputTwo}>2</button>
+          <button onClick={handleInputTwo}>3</button>
+          <button onClick={handleInputTwo}>4</button>
+          <button onClick={handleInputTwo}>5</button>
+          <button onClick={handleInputTwo}>6</button>
+          <button onClick={handleInputTwo}>7</button>
+          <button onClick={handleInputTwo}>8</button>
+          <button onClick={handleInputTwo}>9</button>
+          <button onClick={handleInputTwo}>0</button>
+          <button onClick={clearTwo}>Clear</button>
         </div>
+      </div>
+      <div className="panel answer">
+        <p>{result}</p>
+        <div>
+          <button onClick={handleInputResult}>=</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+// add onClick to numberButton '1'
+
+// take the value and store it in a string
+
+// add onClick to symbolButton '+'
+
+// take the value and store it in a string
+
+// add onClick to numberScndButton '1'
+
+// take the value and store it in a string
+
+// add onClick to equalButton
+
+// using the strings, calculate the result
