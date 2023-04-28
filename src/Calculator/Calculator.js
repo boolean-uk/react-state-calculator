f95f431d07c37610807e424929ac50aed78c4450
Koci H <husnukoci94@gmail.com>
2023-04-28 21:40:40 +0300

    Working on Equation

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -1,66 +1,95 @@
 import { useState } from "react";
 
-function Calculator(props) {
-  // const number = props.number;
+function Calculator() {
+
   const [numbers, setNumbers] = useState("");
 
-  const handleClick = (nums) => {
+//! Panel 1
+  const handleClickOne = (nums) => {
     setNumbers(numbers + nums);
   };
 
-  const handleClear = () => {
+  const handleClearOne = () => {
     setNumbers("");
   }
 
+//! Panel 2
+ const [numbersTwo, setNumbersTwo] = useState("");
+  
+ const handleClickTwo = (numsTwo) => {
+    setNumbersTwo(numbersTwo + numsTwo);
+  };
+
+  const handleClearTwo = () => {
+    setNumbersTwo("");
+  }
+
+  //! Operations
+  const [operation, setOperation] = useState("+");
+  
+ const handleOperatorClick = (opt) => {
+    setOperation(opt);
+  };
+
+  //! Equals
+  const [equation, setEquation] = useState("0");
+  const handleEqualResult = () => {
+    let result = 0
+    if (operation === "+") {
+      result = (numbers + numbersTwo)
+    }
+    setEquation(result);
+  }
+  
   return (
     <div className="calculator">
       <div className="panel">
         <p>{numbers === "" ? 0 : numbers}</p>
         <div className="numbers">
-          <button onClick={() => handleClick("1")}>1</button>
-          <button onClick={() => handleClick("2")}>2</button>
-          <button onClick={() => handleClick("3")}>3</button>
-          <button onClick={() => handleClick("4")}>4</button>
-          <button onClick={() => handleClick("5")}>5</button>
-          <button onClick={() => handleClick("6")}>6</button>
-          <button onClick={() => handleClick("7")}>7</button>
-          <button onClick={() => handleClick("8")}>8</button>
-          <button onClick={() => handleClick("9")}>9</button>
-          <button onClick={() => handleClick("0")}>0</button>
-          <button onClick={() => handleClear()}>Clear</button>
+          <button onClick={() => handleClickOne("1")}>1</button>
+          <button onClick={() => handleClickOne("2")}>2</button>
+          <button onClick={() => handleClickOne("3")}>3</button>
+          <button onClick={() => handleClickOne("4")}>4</button>
+          <button onClick={() => handleClickOne("5")}>5</button>
+          <button onClick={() => handleClickOne("6")}>6</button>
+          <button onClick={() => handleClickOne("7")}>7</button>
+          <button onClick={() => handleClickOne("8")}>8</button>
+          <button onClick={() => handleClickOne("9")}>9</button>
+          <button onClick={() => handleClickOne("0")}>0</button>
+          <button onClick={() => handleClearOne()}>Clear</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>+</p>
-        {/* <div className="numbers">
-          <button onClick={() => click}>+</button>
-          <button onClick={() => click}>-</button>
-          <button onClick={() => click}>*</button>
-          <button onClick={() => click}>÷</button>
-        </div> */}
+        <p>{operation}</p>
+        <div className="numbers">
+          <button onClick={() => handleOperatorClick("+")}>+</button>
+          <button onClick={() => handleOperatorClick("-")}>-</button>
+          <button onClick={() => handleOperatorClick("*")}>*</button>
+          <button onClick={() => handleOperatorClick("/")}>÷</button>
+        </div>
       </div>
 
       <div className="panel">
-        <p>{numbers === "" ? 0 : numbers}</p>
+        <p>{numbersTwo === "" ? 0 : numbersTwo}</p>
         <div className="numbers">
-          <button onClick={() => handleClick("1")}>1</button>
-          <button onClick={() => handleClick("2")}>2</button>
-          <button onClick={() => handleClick("3")}>3</button>
-          <button onClick={() => handleClick("4")}>4</button>
-          <button onClick={() => handleClick("5")}>5</button>
-          <button onClick={() => handleClick("6")}>6</button>
-          <button onClick={() => handleClick("7")}>7</button>
-          <button onClick={() => handleClick("8")}>8</button>
-          <button onClick={() => handleClick("9")}>9</button>
-          <button onClick={() => handleClick("0")}>0</button>
-          <button onClick={() => handleClick("0")}>Clear</button>
+          <button onClick={() => handleClickTwo("1")}>1</button>
+          <button onClick={() => handleClickTwo("2")}>2</button>
+          <button onClick={() => handleClickTwo("3")}>3</button>
+          <button onClick={() => handleClickTwo("4")}>4</button>
+          <button onClick={() => handleClickTwo("5")}>5</button>
+          <button onClick={() => handleClickTwo("6")}>6</button>
+          <button onClick={() => handleClickTwo("7")}>7</button>
+          <button onClick={() => handleClickTwo("8")}>8</button>
+          <button onClick={() => handleClickTwo("9")}>9</button>
+          <button onClick={() => handleClickTwo("0")}>0</button>
+          <button onClick={() => handleClearTwo("0")}>Clear</button>
         </div>
       </div>
       <div className="panel answer">
-        <p>0</p>
+        <p>{equation}</p>
         <div>
-          <button>=</button>
+          <button onClick={() => handleEqualResult()}>=</button>
         </div>
       </div>
     </div>
