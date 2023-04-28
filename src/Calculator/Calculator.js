ecfb1ea8d9417b2021e90a63c14c563832d6ce3f
Isabelle <isabel.tartarelli@live.it>
2023-04-28 18:16:22 +0100

    Clear button working

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -8,49 +8,53 @@ function Calculator(props) {
     setNumbers(numbers + nums);
   };
 
+  const handleClear = () => {
+    setNumbers("");
+  }
+
   return (
     <div className="calculator">
       <div className="panel">
         <p>{numbers === "" ? 0 : numbers}</p>
         <div className="numbers">
           <button onClick={() => handleClick("1")}>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={() => handleClick("2")}>2</button>
+          <button onClick={() => handleClick("3")}>3</button>
+          <button onClick={() => handleClick("4")}>4</button>
+          <button onClick={() => handleClick("5")}>5</button>
+          <button onClick={() => handleClick("6")}>6</button>
+          <button onClick={() => handleClick("7")}>7</button>
+          <button onClick={() => handleClick("8")}>8</button>
+          <button onClick={() => handleClick("9")}>9</button>
+          <button onClick={() => handleClick("0")}>0</button>
+          <button onClick={() => handleClear()}>Clear</button>
         </div>
       </div>
 
       <div className="panel">
         <p>+</p>
-        <div className="numbers">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
-        </div>
+        {/* <div className="numbers">
+          <button onClick={() => click}>+</button>
+          <button onClick={() => click}>-</button>
+          <button onClick={() => click}>*</button>
+          <button onClick={() => click}>÷</button>
+        </div> */}
       </div>
 
       <div className="panel">
-        <p>0</p>
+        <p>{numbers === "" ? 0 : numbers}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={() => handleClick("1")}>1</button>
+          <button onClick={() => handleClick("2")}>2</button>
+          <button onClick={() => handleClick("3")}>3</button>
+          <button onClick={() => handleClick("4")}>4</button>
+          <button onClick={() => handleClick("5")}>5</button>
+          <button onClick={() => handleClick("6")}>6</button>
+          <button onClick={() => handleClick("7")}>7</button>
+          <button onClick={() => handleClick("8")}>8</button>
+          <button onClick={() => handleClick("9")}>9</button>
+          <button onClick={() => handleClick("0")}>0</button>
+          <button onClick={() => handleClick("0")}>Clear</button>
         </div>
       </div>
       <div className="panel answer">
