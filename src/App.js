7cde5f2ff0963aad399dcdd9de1e6ef637c6ce02
Mantas Navickis <navickis@gmail.com>
2023-05-09 11:59:45 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,58 +1,106 @@
 import "./App.css"
+import { useState } from "react"
 
-function App() { 
+function App() {
+  const [numberOne, setNumberOne] = useState(0)
+  const [symbol, setsymbol] = useState("+")
+  const [numberTwo, setNumberTwo] = useState(0)
+  const [answer, setAnswer] = useState(0)
+
+  const handleNumberOne = (e) => {
+    const input = e.target.innerText
+
+    if (numberOne === 0) {
+      setNumberOne(input)
+      console.log(setNumberOne)
+      return
+    } else {
+      setNumberOne(numberOne + input)
+      return
+    }
+  }
+
+  const handleNumberTwo = (e) => {
+    const input = e.target.innerText
+
+    if (numberTwo === 0) {
+      setNumberTwo(input)
+    } else {
+      setNumberTwo(numberTwo + input)
+    }
+  }
+  function calculate() {
+    let numberOneNum = Number(numberOne)
+    let numberTwoNum = Number(numberTwo)
+
+    if (symbol === "+") {
+      setAnswer(numberOneNum + numberTwoNum)
+      console.log(setAnswer)
+    }
+    else if (symbol === "-") {
+      setAnswer(numberOneNum - numberTwoNum)
+    } else if (symbol === "*") {
+      setAnswer(numberOneNum * numberTwoNum)
+    }
+    else if (symbol === "÷") {
+      setAnswer(numberOneNum / numberTwoNum)
+    }
+  }
+
 
   return (
     <div className="calculator">
-        <div className="panel">
-          <p>0</p>
-          <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
-          </div>
+      <div className="panel">
+        <p>{numberOne}</p>
+        <div className="numbers">
+          <button onClick={handleNumberOne}>1</button>
+          <button onClick={handleNumberOne}>2</button>
+          <button onClick={handleNumberOne}>3</button>
+          <button onClick={handleNumberOne}>4</button>
+          <button onClick={handleNumberOne}>5</button>
+          <button onClick={handleNumberOne}>6</button>
+          <button onClick={handleNumberOne}>7</button>
+          <button onClick={handleNumberOne}>8</button>
+          <button onClick={handleNumberOne}>9</button>
+          <button onClick={handleNumberOne}>0</button>
+          <button onClick={() => setNumberOne(0)}>Clear</button>
+          <button>Recall</button>
         </div>
-        
-        <div className="panel">
-          <p>+</p>
-          <div className="numbers">
-            <button>+</button>
-            <button>-</button>
-            <button>*</button>
-            <button>÷</button>
-          </div>
-        </div>
+      </div>
 
-        <div className="panel">
-          <p>0</p>
-          <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
-          </div>
+      <div className="panel">
+        <p>{symbol}</p>
+        <div className="numbers">
+          <button onClick={() => setsymbol("+")}>+</button>
+          <button onClick={() => setsymbol("-")}>-</button>
+          <button onClick={() => setsymbol("*")}>*</button>
+          <button onClick={() => setsymbol("÷")}>÷</button>
         </div>
-        <div className="panel answer">
-          <p>0</p>
-          <div>
-            <button>=</button>
-          </div>
+      </div>
+
+      <div className="panel">
+        <p>{numberTwo}</p>
+        <div className="numbers">
+          <button onClick={handleNumberTwo}>1</button>
+          <button onClick={handleNumberTwo}>2</button>
+          <button onClick={handleNumberTwo}>3</button>
+          <button onClick={handleNumberTwo}>4</button>
+          <button onClick={handleNumberTwo}>5</button>
+          <button onClick={handleNumberTwo}>6</button>
+          <button onClick={handleNumberTwo}>7</button>
+          <button onClick={handleNumberTwo}>8</button>
+          <button onClick={handleNumberTwo}>9</button>
+          <button onClick={handleNumberTwo}>0</button>
+          <button onClick={() => setNumberTwo(0)}>Clear</button>
+          <button>Recall</button>
         </div>
+      </div>
+      <div className="panel answer">
+        <p>{answer}</p>
+        <div>
+          <button onClick={() => calculate()}>=</button>
+        </div>
+      </div>
     </div>
   )
 }
