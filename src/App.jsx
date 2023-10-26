deb17e95b3d19c1c1d9593bb42b6ca5cb081aacb
Papi <test@Tests-Air.fritz.box>
2023-10-26 23:44:39 +0200

    Papi

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,93 @@
+import { useState } from "react"
 import "./App.css"
 
 function App() {
+  const [numOne, setNumOne] = useState(0);
+  const [numTwo, setNumTwo] = useState(0);
+  const [operator, setOperator] = useState("+");
+  const [answer, setAnswer] = useState(0);
+
+  const numOneClick = (e) => {
+    setNumOne(e.target.innerText)
+  };
+
+  const numTwoClick = (e) => {
+    setNumTwo(e.target.innerText)
+  };
+
+  const opClick = (e) => {
+    setOperator(e.target.innerText)
+  };
+
+  const calc = () => {
+    switch(operator) {
+      case "+":
+        setAnswer(Number(numOne) + Number(numTwo));
+        break;
+      case "*":
+        setAnswer(numOne * numTwo);
+        break;
+      case "-":
+        setAnswer(numOne - numTwo);
+        break;
+      case "รท":
+        setAnswer(numOne / numTwo);
+        break;
+      default: 
+        setAnswer(0)
+        break;
+    }
+  }
 
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
+        <p>{numOne}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={() => setNumOne(1)}>1</button>
+          <button onClick={numOneClick}>2</button>
+          <button onClick={numOneClick}>3</button>
+          <button onClick={numOneClick}>4</button>
+          <button onClick={numOneClick}>5</button>
+          <button onClick={numOneClick}>6</button>
+          <button onClick={numOneClick}>7</button>
+          <button onClick={numOneClick}>8</button>
+          <button onClick={numOneClick}>9</button>
+          <button onClick={numOneClick}>0</button>
+          <button onClick={() => setNumOne(0)}>Clear</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>+</p>
+        <p>{operator}</p>
         <div className="numbers">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
+          <button onClick={opClick}>+</button>
+          <button onClick={opClick}>-</button>
+          <button onClick={opClick}>*</button>
+          <button onClick={opClick}>รท</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>0</p>
+        <p>{numTwo}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={numTwoClick}>1</button>
+          <button onClick={numTwoClick}>2</button>
+          <button onClick={numTwoClick}>3</button>
+          <button onClick={numTwoClick}>4</button>
+          <button onClick={numTwoClick}>5</button>
+          <button onClick={numTwoClick}>6</button>
+          <button onClick={numTwoClick}>7</button>
+          <button onClick={numTwoClick}>8</button>
+          <button onClick={numTwoClick}>9</button>
+          <button onClick={numTwoClick}>0</button>
+          <button onClick={() => setNumTwo("0")}>Clear</button>
         </div>
       </div>
       <div className="panel answer">
-        <p>0</p>
+        <p>{answer}</p>
         <div>
-          <button>=</button>
+          <button onClick={calc}>=</button>
         </div>
       </div>
     </div>
