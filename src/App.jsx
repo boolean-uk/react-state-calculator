51644074eaef33a252153e8425a76e65bc83b2ca
aayushlama4008 <aayushlama4008@gmail.com>
2023-10-27 00:04:15 +0200

    core done

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,96 @@
+import { useState } from "react";
 import "./App.css"
 
 function App() {
+  //varibles 
+  const [firstNumber , setFirstNumber] = useState('0')
+  const [secondNumber , setSecondNumber] = useState('0')
+  const [operator, setOperator] = useState('+')
+  const [result , setResult] = useState('0')
+    //function to make the event 
+    function first(event) {
+      if(firstNumber === '0'){
+        setFirstNumber(event.target.innerText)
+      }else {
+        setFirstNumber(firstNumber + event.target.innerText)
+      }
+    }
+
+    //for the second number
+    function second(event) {
+      if(secondNumber === '0'){
+        setSecondNumber(event.target.innerText)
+      }else {
+        setSecondNumber(secondNumber + event.target.innerText)
+      }
+    }
+
+    //function for the result
+    function totalResult(){
+      if(operator === '+'){
+        setResult(parseInt(firstNumber)+ parseInt(secondNumber))
+      }else if (operator === '-'){
+        setResult(parseInt(firstNumber)- parseInt(secondNumber))
+      }else if (operator === '*'){
+        setResult(parseInt(firstNumber)* parseInt(secondNumber))
+      }else if (operator === '/'){
+        setResult(parseInt(firstNumber)/ parseInt(secondNumber))
+      }
+    }
 
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
+        <p>{firstNumber}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={event =>first(event)}>1</button>
+          <button onClick={event =>first(event)}>2</button>
+          <button onClick={event =>first(event)}>3</button>
+          <button onClick={event =>first(event)}>4</button>
+          <button onClick={event =>first(event)}>5</button>
+          <button onClick={event =>first(event)}>6</button>
+          <button onClick={event =>first(event)}>7</button>
+          <button onClick={event =>first(event)}>8</button>
+          <button onClick={event =>first(event)}>9</button>
+          <button onClick={event =>first(event)}>0</button>
+          <button onClick={()=>setFirstNumber('0')}>Clear</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>+</p>
+        <p>{operator}</p>
         <div className="numbers">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
+          <button onClick={(event)=>setOperator(event.target.innerText)}>-</button>
+          <button onClick={(event)=>setOperator(event.target.innerText)}>+</button>
+          <button onClick={(event)=>setOperator(event.target.innerText)}>*</button>
+          <button onClick={(event)=>setOperator(event.target.innerText)}>÷</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>0</p>
+        <p>{secondNumber}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={event =>second(event)}>1</button>
+          <button onClick={event =>second(event)}>2</button>
+          <button onClick={event =>second(event)}>3</button>
+          <button onClick={event =>second(event)}>4</button>
+          <button onClick={event =>second(event)}>5</button>
+          <button onClick={event =>second(event)}>6</button>
+          <button onClick={event =>second(event)}>7</button>
+          <button onClick={event =>second(event)}>8</button>
+          <button onClick={event =>second(event)}>9</button>
+          <button onClick={event =>second(event)}>0</button>
+          <button onClick={()=>setSecondNumber('0')}>Clear</button>
         </div>
       </div>
       <div className="panel answer">
-        <p>0</p>
+        <p>{result}</p>
         <div>
-          <button>=</button>
+          <button onClick={() => totalResult()}>=</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default App
