7c19e0fa249293ce593918c6c01b190989bf4240
Satoki <satokiito@gmail.com>
2023-10-26 15:56:33 +0100

    core - first number panel functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react"
+
 import "./App.css"
 
 function App() {
+const [firstNum, setFirstNum] = useState('0')
+// const [operation, setOperation] = useState('')
+// const [secondNum, setSecondNum] = useState(0)
+// const [result, setResult] = useState(0)
+
+function firstNumFull(e) {
+  if (firstNum === '0') {
+    setFirstNum(e.target.innerText)
+  }
+  else {
+    setFirstNum(firstNum + e.target.innerText)
+  }
+}
 
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
-        <div className="numbers">
+        <p>{firstNum}</p>
+        <div className="numbers" onClick={e => firstNumFull(e)}>
           <button>1</button>
           <button>2</button>
           <button>3</button>
