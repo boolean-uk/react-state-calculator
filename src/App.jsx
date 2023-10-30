9f633f3c4b203ba009b14ac9f76c6fb79808035d
tomlukrprogram <tomlukeprogramming@outlook.com>
2023-10-30 13:01:14 +0000

    core

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,113 @@
-import { useState } from "react"
-import "./App.css"
-
-
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [display, setDisplay] = useState("0");
   const [operation, setOperation] = useState("");
   const [firstOperand, setFirstOperand] = useState("");
-  const [secondOperand, setsecondOperand] = useState("");
+  const [secondOperand, setSecondOperand] = useState("");
   const [answer, setAnswer] = useState("0");
+
+  const handleNumberClick = (number) => {
+    if (operation === "") {
+      setFirstOperand((prev) => prev + number);
+      setDisplay((prev) => (prev === "0" ? number : prev + number));
+    } else {
+      setSecondOperand((prev) => prev + number);
+      setDisplay((prev) => prev === "0" ? number : prev + number);
+    }
+  };
+
+  const handleOperationClick = (op) => {
+    setOperation(op);
+    setDisplay(op);
+  };
+
+  const handleClearClick = () => {
+    setDisplay("0");
+    setFirstOperand("");
+    setSecondOperand("");
+    setOperation("");
+    setAnswer("0");
+  };
+
+  const handleEqualsClick = () => {
+    const num1 = parseFloat(firstOperand);
+    const num2 = parseFloat(secondOperand);
+    let result;
+
+    if (operation === "÷" && num2 === 0) {
+      result = "Error";
+    } else {
+      switch (operation) {
+        case "+":
+          result = num1 + num2;
+          break;
+        case "-":
+          result = num1 - num2;
+          break;
+        case "*":
+          result = num1 * num2;
+          break;
+        case "÷":
+          result = num1 / num2;
+          break;
+        default:
+          result = num1;
+      }
+    }
+
+    setAnswer(result.toString());
+    setDisplay(result.toString());
+    setFirstOperand("");
+    setSecondOperand("");
+    setOperation("");
+  };
 
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
+        <p>{display}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((number) => (
+            <button key={number} onClick={() => handleNumberClick(number)}>
+              {number}
+            </button>
+          ))}
+          <button onClick={handleClearClick}>Clear</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>+</p>
+        <p>{operation}</p>
         <div className="numbers">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
+          {["+", "-", "*", "÷"].map((op) => (
+            <button key={op} onClick={() => handleOperationClick(op)}>
+              {op}
+            </button>
+          ))}
         </div>
       </div>
 
       <div className="panel">
-        <p>0</p>
+        <p>{answer}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((number) => (
+            <button key={number} onClick={() => handleNumberClick(number)}>
+              {number}
+            </button>
+          ))}
+          <button onClick={handleClearClick}>Clear</button>
         </div>
       </div>
       <div className="panel answer">
-        <p>0</p>
+        <p>{answer}</p>
         <div>
-          <button>=</button>
+          <button onClick={handleEqualsClick}>=</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
