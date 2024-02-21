57897cca81e7c02d66a4518ea0f76cdddbf826c2
Klara Andersson <klara.andersson@se.experis.com>
2024-02-21 12:49:26 +0100

    Core

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,85 @@
+import { useState } from "react"
 import "./App.css"
 
 function App() {
 
+  const [firstNumber, setFirstNumber] = useState(0)
+  const [operation, setOperation] = useState('+')
+  const [secondNumber, setSecondNumber] = useState(0)
+  const [answerNumber, setAnswer] = useState(0)
+
+  const changeFirstNumber = (number) =>
+  {
+    setFirstNumber(number)
+  }
+
+  const changeOperation = (op) =>
+  {
+    setOperation(op)
+  }
+
+  const changeSecondNumber = (number) =>
+  {
+    setSecondNumber(number)
+  }
+
+  const calculate = () =>
+  {
+    if (operation === '+') setAnswer(firstNumber + secondNumber)
+    if (operation === '-') setAnswer(firstNumber - secondNumber)
+    if (operation === '*') setAnswer(firstNumber * secondNumber)
+    if (operation === '÷') setAnswer(firstNumber / secondNumber)
+  }
+
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
+        <p>{firstNumber}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={() => changeFirstNumber(1)}>1</button>
+          <button onClick={() => changeFirstNumber(2)}>2</button>
+          <button onClick={() => changeFirstNumber(3)}>3</button>
+          <button onClick={() => changeFirstNumber(4)}>4</button>
+          <button onClick={() => changeFirstNumber(5)}>5</button>
+          <button onClick={() => changeFirstNumber(6)}>6</button>
+          <button onClick={() => changeFirstNumber(7)}>7</button>
+          <button onClick={() => changeFirstNumber(8)}>8</button>
+          <button onClick={() => changeFirstNumber(9)}>9</button>
+          <button onClick={() => changeFirstNumber(0)}>0</button>
+          <button onClick={() => changeFirstNumber(0)}>Clear</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>+</p>
+        <p>{operation}</p>
         <div className="numbers">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
+          <button onClick={() => changeOperation('+')}>+</button>
+          <button onClick={() => changeOperation('-')}>-</button>
+          <button onClick={() => changeOperation('*')}>*</button>
+          <button onClick={() => changeOperation('÷')}>÷</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>0</p>
+        <p>{secondNumber}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={() => changeSecondNumber(1)}>1</button>
+          <button onClick={() => changeSecondNumber(2)}>2</button>
+          <button onClick={() => changeSecondNumber(3)}>3</button>
+          <button onClick={() => changeSecondNumber(4)}>4</button>
+          <button onClick={() => changeSecondNumber(5)}>5</button>
+          <button onClick={() => changeSecondNumber(6)}>6</button>
+          <button onClick={() => changeSecondNumber(7)}>7</button>
+          <button onClick={() => changeSecondNumber(8)}>8</button>
+          <button onClick={() => changeSecondNumber(9)}>9</button>
+          <button onClick={() => changeSecondNumber(0)}>0</button>
+          <button onClick={() => changeSecondNumber(0)}>Clear</button>
         </div>
       </div>
       <div className="panel answer">
-        <p>0</p>
+        <p>{answerNumber}</p>
         <div>
-          <button>=</button>
+          <button onClick={calculate}>=</button>
         </div>
       </div>
     </div>
