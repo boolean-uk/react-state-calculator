52b0a5a6bdf0a3c346b65a88d8bc85c99c72f9e5
Annemoon_de_Groen <annemoon2003@gmail.com>
2024-02-21 15:35:21 +0100

    Finished all extensions

--- src/Numbers.jsx
+++ src/Numbers.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 function Numbers({state, ans})
 {
     const [currentState, newState] = state
+    const [isBeforeSeparator, toggleBeforeSeparator] = useState(true)
     const changeState = (number) => {
         if (currentState === 0) {
             newState(number)
@@ -11,6 +12,7 @@ function Numbers({state, ans})
     }
 
     const clear = () =>{
+        toggleBeforeSeparator(true)
         newState(0)
     }
 
@@ -19,15 +21,24 @@ function Numbers({state, ans})
         buttonArray.push(<button onClick={() => changeState(i)}>{i}</button>)
     }
     const recall = () => {
+        toggleBeforeSeparator(Number.isInteger(ans))
         newState(ans)
     }
 
+    const addSeparator = () =>{
+        //Checks if there is already a separator, if yes, returns without doing anything
+        if (!isBeforeSeparator) return
+        toggleBeforeSeparator(false)
+        newState(`${currentState}.`)
+        //
+    }
     return (
         <div className="panel">
         <p>{currentState}</p>
         <div className="numbers">
             {buttonArray}
             <button onClick={() => changeState(0)}>0</button>
+            <button onClick={() => addSeparator()}>.</button>
             <button onClick={() => clear()}>Clear</button>
             <button onClick={() => recall()}> Recall</button>
         </div>
