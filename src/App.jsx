71e31e3868a8cf4726ddc796536f5843b7ed7f17
Stian Gaustad <stian.gaustad@no.experis.com>
2024-02-22 13:27:25 +0100

    Core - Resolved core functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,108 @@
 import "./App.css"
+import { useState } from 'react'
 
 function App() {
+  const [firstNumber, setFirstNumber] = useState("0")
+  const [secondNumber, setSecondNumber] = useState("0")
+  const [operator, setOperator] = useState("+")
+  const [result, setResult] = useState(0)
+
+  const ChangeFirstNumber = (e) => {
+    if (e.target.innerText === "Clear") {
+      setFirstNumber("0")
+    } else {
+      if (firstNumber === "0") {
+        setFirstNumber(e.target.innerText)
+      } else {
+        setFirstNumber(firstNumber + e.target.innerText)
+      }
+    }
+  }
+
+  const ChangeSecondNumber = (e) => {
+    if (e.target.innerText === "Clear") {
+      setSecondNumber("0")
+    } else {
+      if (secondNumber === "0") {
+        setSecondNumber(e.target.innerText)
+      } else {
+        setSecondNumber(secondNumber + e.target.innerText)
+      }
+    }
+  }
+
+  const ChangeOperator = (e) => {
+    setOperator(e.target.innerText)
+  }
+
+  const EvaluateResult = () => {
+    let calculationValue = 0
+    switch (operator) {
+      case "+":
+        calculationValue = parseInt(firstNumber) + parseInt(secondNumber)
+        break
+      case "-":
+        calculationValue = parseInt(firstNumber) - parseInt(secondNumber)
+        break
+      case "*":
+        calculationValue = parseInt(firstNumber) * parseInt(secondNumber)
+        break
+      case "÷":
+        calculationValue = parseInt(firstNumber) / parseInt(secondNumber)
+        break
+    }
+    setResult(calculationValue)
+  }
 
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
+        <p>{firstNumber}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={(e) => ChangeFirstNumber(e)}>1</button>
+          <button onClick={(e) => ChangeFirstNumber(e)}>2</button>
+          <button onClick={(e) => ChangeFirstNumber(e)}>3</button>
+          <button onClick={(e) => ChangeFirstNumber(e)}>4</button>
+          <button onClick={(e) => ChangeFirstNumber(e)}>5</button>
+          <button onClick={(e) => ChangeFirstNumber(e)}>6</button>
+          <button onClick={(e) => ChangeFirstNumber(e)}>7</button>
+          <button onClick={(e) => ChangeFirstNumber(e)}>8</button>
+          <button onClick={(e) => ChangeFirstNumber(e)}>9</button>
+          <button onClick={(e) => ChangeFirstNumber(e)}>0</button>
+          <button onClick={(e) => ChangeFirstNumber(e)}>Clear</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>+</p>
+        <p>{operator}</p>
         <div className="numbers">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
+          <button onClick={(e) => ChangeOperator(e)}>+</button>
+          <button onClick={(e) => ChangeOperator(e)}>-</button>
+          <button onClick={(e) => ChangeOperator(e)}>*</button>
+          <button onClick={(e) => ChangeOperator(e)}>÷</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>0</p>
+        <p>{secondNumber}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={(e) => ChangeSecondNumber(e)}>1</button>
+          <button onClick={(e) => ChangeSecondNumber(e)}>2</button>
+          <button onClick={(e) => ChangeSecondNumber(e)}>3</button>
+          <button onClick={(e) => ChangeSecondNumber(e)}>4</button>
+          <button onClick={(e) => ChangeSecondNumber(e)}>5</button>
+          <button onClick={(e) => ChangeSecondNumber(e)}>6</button>
+          <button onClick={(e) => ChangeSecondNumber(e)}>7</button>
+          <button onClick={(e) => ChangeSecondNumber(e)}>8</button>
+          <button onClick={(e) => ChangeSecondNumber(e)}>9</button>
+          <button onClick={(e) => ChangeSecondNumber(e)}>0</button>
+          <button onClick={(e) => ChangeSecondNumber(e)}>Clear</button>
         </div>
       </div>
       <div className="panel answer">
-        <p>0</p>
+        <p>{result}</p>
         <div>
-          <button>=</button>
+          <button onClick={() => EvaluateResult()}>=</button>
         </div>
       </div>
     </div>
