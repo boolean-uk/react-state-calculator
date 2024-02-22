2ca611518df931de4fc0dd600e87c924027ef6b9
llllllll-l <vincent.werelius@gmail.com>
2024-02-22 14:41:23 +0100

    core done

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,130 @@
-import "./App.css"
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [leftDisplay, setLeftDisplay] = useState("0");
+  const [rightDisplay, setRightDisplay] = useState("0");
+
+  const [operatorDisplay, setOperator] = useState("");
+
+  const [resultDisplay, setResult] = useState("0");
+
+  const handleNumberClick = (number, panel) => {
+    if (panel === "left") {
+      setLeftDisplay((prevInput) =>
+        prevInput === "0" ? number : prevInput + number
+      );
+    } else if (panel === "right") {
+      setRightDisplay((prevInput) =>
+        prevInput === "0" ? number : prevInput + number
+      );
+    }
+  };
+
+  const handleClear = (panel) => {
+    if (panel === "left") {
+      setLeftDisplay("0");
+    } else if (panel === "right") {
+      setRightDisplay("0");
+    }
+  };
+
+  const handleOperationClick = (operation) => {
+    switch (operation) {
+      case "+":
+        setOperator(operation);
+        break;
+      case "-":
+        setOperator(operation);
+        break;
+      case "*":
+        setOperator(operation);
+        break;
+      case "÷":
+        setOperator(operation);
+        break;
+      default:
+        break;
+    }
+    console.log(operation);
+  };
+
+  const culateTotal = () => {
+    const leftNum = parseFloat(leftDisplay);
+    const rightNum = parseFloat(rightDisplay);
+
+    switch (operatorDisplay) {
+      case "+":
+        setResult(String(leftNum + rightNum));
+        break;
+      case "-":
+        setResult(String(leftNum - rightNum));
+        break;
+      case "*":
+        setResult(String(leftNum * rightNum));
+        break;
+      case "÷":
+        setResult(String(leftNum / rightNum));
+        break;
+      default:
+        break;
+    }
+    setLeftDisplay("0");
+    setRightDisplay("0");
+    setOperator("");
+  };
 
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
+        <p>{leftDisplay}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((number) => (
+            <button
+              key={number}
+              onClick={() => handleNumberClick(String(number), "left")}
+            >
+              {number}
+            </button>
+          ))}
+          <button onClick={() => handleClear("left")}>Clear</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>+</p>
+        <p>{operatorDisplay}</p>
         <div className="numbers">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
+          {["+", "-", "*", "÷"].map((op) => (
+            <button key={op} onClick={() => handleOperationClick(op)}>
+              {op}
+            </button>
+          ))}
         </div>
       </div>
 
       <div className="panel">
-        <p>0</p>
+        <p>{rightDisplay}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((number) => (
+            <button
+              key={number}
+              onClick={() => handleNumberClick(String(number), "right")}
+            >
+              {number}
+            </button>
+          ))}
+          <button onClick={() => handleClear("right")}>Clear</button>
         </div>
       </div>
       <div className="panel answer">
-        <p>0</p>
+        <p>{resultDisplay}</p>
         <div>
-          <button>=</button>
+          <button onClick={culateTotal}>=</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
