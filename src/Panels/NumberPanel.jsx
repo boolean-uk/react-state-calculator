58d4954ef46a2a811e19c9c4ef6b175f85f03ab3
Ole Markus Roland <ole.markus.roland@no.experis.com>
2024-02-23 11:19:51 +0100

    Completed Extension 3

--- src/Panels/NumberPanel.jsx
+++ src/Panels/NumberPanel.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
+
 export const NumberPanel = ({ num, setNum, saved }) => {
-  const handleButtonClick = (number) => {
-    if(num === 0)
-      setNum(number)
+  const [hasDecimal, setHasDecimal] = useState(false)
+
+  const handleNumberClick = (clickedNum) => {
+    if(num === 0 && !hasDecimal)
+      setNum(clickedNum)
     else {
-      setNum(Number(num.toString() + number.toString()));
+      setNum(parseFloat(num.toString() + clickedNum.toString()));
     }
   };
 
-  const handleClearClick = () => {
+  const addDecimal = () =>{
+    if (!hasDecimal){
+      setHasDecimal(true)
+      setNum(num + '.')
+    }
+  }
+
+  const clear = () => {
     setNum(0);
+    setHasDecimal(false)
   };
 
   const loadSaved = () => {
@@ -18,14 +30,15 @@ export const NumberPanel = ({ num, setNum, saved }) => {
 
   return (
     <div className="panel">
-      <p>{num}</p>
+      <p>{num}{(hasDecimal && Number.isInteger(num)) ? '.' : null}</p>
       <div className="numbers">
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((number) => (
-          <button key={number} onClick={() => handleButtonClick(number)}>
+          <button key={number} onClick={() => handleNumberClick(number)}>
             {number}
           </button>
         ))}
-        <button onClick={handleClearClick}>Clear</button>
+        <button onClick={addDecimal}>.</button>
+        <button onClick={clear}>Clear</button>
         <button onClick={loadSaved}>Load stored</button>
       </div>
     </div>
