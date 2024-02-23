6c225fb9f2cea99e883293085606d748aa335521
Jens Arvid Forgård <jensarvid99@gmail.com>
2024-02-23 10:28:51 +0100

    Core + Extension 1 and 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,97 @@
 import "./App.css"
+import { useState } from "react";
 
 function App() {
+  const [firstNumber, SetFirstNumber] = useState("0");
+  const [operator, SetOperator] = useState("+");
+  const [secondNumber, SetSecondNumber] = useState("0");
+  const [answer, SetAnswer] = useState("0");
+  const [storedValue, SetStoredValue] = useState("0");
 
+
+  const AppendFirstNumber = (number) => {
+    SetFirstNumber(prevFirstNumber => {
+      if (prevFirstNumber === "0") {
+        return "" + number;
+      } else {
+        return prevFirstNumber + number;
+      }
+    });
+  };
+
+  const AppendSecondNumber = (number) => {
+    SetSecondNumber(prevSecondNumber => {
+      if (prevSecondNumber === "0") {
+        return "" + number;
+      } else {
+        return prevSecondNumber + number;
+      }
+    });
+  };
+
+  const CalculateAnswer = () => {
+    let firstValue = parseFloat(firstNumber);
+    let secondValue = parseFloat(secondNumber);
+
+    if (operator === "+") {
+      SetAnswer((firstValue + secondValue).toString());
+    } else if (operator === "-") {
+      SetAnswer((firstValue - secondValue).toString());
+    } else if (operator === "*") {
+      SetAnswer((firstValue * secondValue).toString());
+    } else if (operator === "÷") {
+      SetAnswer((firstValue / secondValue).toFixed());
+    }
+  };
+
+  
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
+        <p>{firstNumber}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((number) => (
+            <button key={number} onClick={() => AppendFirstNumber(number)}>
+              {number}
+            </button>
+          ))}
+          <button onClick={() => SetFirstNumber("0")}>Clear</button>
+          <button onClick={() => SetFirstNumber(storedValue)}>Recall</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>+</p>
+        <p>{operator}</p>
         <div className="numbers">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
+        {["+", "-", "*", "÷"].map((op) => (
+            <button key={op} onClick={() => SetOperator(op)}>
+              {op}
+            </button>
+          ))}
         </div>
       </div>
 
       <div className="panel">
-        <p>0</p>
+        <p>{secondNumber}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((number) => (
+            <button key={number} onClick={() => AppendSecondNumber(number)}>
+              {number}
+            </button>
+          ))}
+          <button onClick={() => SetSecondNumber("0")}>Clear</button>
+          <button onClick={() => SetSecondNumber(storedValue)}>Recall</button>
         </div>
       </div>
       <div className="panel answer">
-        <p>0</p>
+        <p>{answer}</p>
         <div>
-          <button>=</button>
+        <button onClick={CalculateAnswer}>=</button>
+        <button onClick={() => SetStoredValue(answer)}>Store</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
