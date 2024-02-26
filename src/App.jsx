f4859afb5badb0a097990ec91daf84955adc903e
Maha Riazi <maha.riazi@hotmail.no>
2024-02-26 12:44:25 +0100

    core complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,70 @@
+import { useState } from "react"
 import "./App.css"
 
 function App() {
+  const [num1, setNum1] = useState(0)
+  const [operation, setOperation] = useState('+')
+  const [num2, setNum2] = useState(0)
+  const [sum, setSum] = useState(0)
 
+  function applyOperation(){
+    setSum(eval(`${num1} ${operation} ${num2}`))
+  }
+  
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
+        <p>{num1}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={() => setNum1(1)}>1</button>
+          <button onClick={() => setNum1(2)}>2</button>
+          <button onClick={() => setNum1(3)}>3</button>
+          <button onClick={() => setNum1(4)}>4</button>
+          <button onClick={() => setNum1(5)}>5</button>
+          <button onClick={() => setNum1(6)}>6</button>
+          <button onClick={() => setNum1(7)}>7</button>
+          <button onClick={() => setNum1(8)}>8</button>
+          <button onClick={() => setNum1(9)}>9</button>
+          <button onClick={() => setNum1(0)}>0</button>
+          <button onClick={() => setNum1(0)}>Clear</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>+</p>
+        <p>{operation}</p>
         <div className="numbers">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
+          <button onClick={() => setOperation("+")}>+</button>
+          <button onClick={() => setOperation("-")}>-</button>
+          <button onClick={() => setOperation("*")}>*</button>
+          <button onClick={() => setOperation("/")}>÷</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>0</p>
+        <p>{num2}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={() => setNum2(1)}>1</button>
+          <button onClick={() => setNum2(2)}>2</button>
+          <button onClick={() => setNum2(3)}>3</button>
+          <button onClick={() => setNum2(4)}>4</button>
+          <button onClick={() => setNum2(5)}>5</button>
+          <button onClick={() => setNum2(6)}>6</button>
+          <button onClick={() => setNum2(7)}>7</button>
+          <button onClick={() => setNum2(8)}>8</button>
+          <button onClick={() => setNum2(9)}>9</button>
+          <button onClick={() => setNum2(0)}>0</button>
+          <button onClick={() => setNum2(0)}>Clear</button>
         </div>
       </div>
+
       <div className="panel answer">
-        <p>0</p>
+        <p>{sum}</p>
         <div>
-          <button>=</button>
+          <button onClick={applyOperation}>=</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default App
