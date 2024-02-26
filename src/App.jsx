e09a756e44da836a57fc4bff247f73a711647d73
Knut S Rostad <knut.rostad@no.experis.com>
2024-02-26 13:06:33 +0100

    Core done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,64 @@
 import "./App.css"
+import { useState } from 'react'
 
 function App() {
+  const [field1, setField1] = useState(0)
+  const [field2, setField2] = useState('+')
+  const [field3, setField3] = useState(0)
+  const [field4, setField4] = useState(0)
+
+  const handleClick1 = (event) =>{
+    if (event.target.tagName === 'BUTTON') {
+      if(event.target.innerText === 'Clear') {
+        setField1(0)
+      }else{
+        setField1(event.target.innerText)
+      }
+      
+    }
+  }
+
+  const handleClick2 = (event) =>{
+    if (event.target.tagName === 'BUTTON') {
+      setField2(event.target.innerText)
+    }
+  }
+
+  const handleClick3 = (event) =>{
+    if (event.target.tagName === 'BUTTON') {
+      if(event.target.innerText === 'Clear') {
+        setField3(0)
+      }else{
+        setField3(event.target.innerText)
+      }
+      
+    }
+  }
+
+  const handleClick4 = (event) =>{
+    if (event.target.tagName === 'BUTTON') {
+      let answer = 0
+      const parsed1 = parseFloat(field1)
+      const parsed3 = parseFloat(field3)
+      if(field2 === '+'){
+        answer = parsed1+parsed3
+      }else if(field2 === '-'){
+        answer = parsed1-parsed3
+      }else if(field2 === '÷'){
+        answer = parsed1/parsed3
+      }else if(field2 === '*'){
+        answer = parsed1*parsed3
+      }
+      setField4(answer)
+    }
+  }
+
 
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
-        <div className="numbers">
+        <p>{field1}</p>
+        <div className="numbers" onClick={handleClick1}>
           <button>1</button>
           <button>2</button>
           <button>3</button>
@@ -22,8 +74,8 @@ function App() {
       </div>
 
       <div className="panel">
-        <p>+</p>
-        <div className="numbers">
+        <p>{field2}</p>
+        <div className="numbers" onClick={handleClick2}>
           <button>+</button>
           <button>-</button>
           <button>*</button>
@@ -32,8 +84,8 @@ function App() {
       </div>
 
       <div className="panel">
-        <p>0</p>
-        <div className="numbers">
+        <p>{field3}</p>
+        <div className="numbers" onClick={handleClick3}>
           <button>1</button>
           <button>2</button>
           <button>3</button>
@@ -47,8 +99,8 @@ function App() {
           <button>Clear</button>
         </div>
       </div>
-      <div className="panel answer">
-        <p>0</p>
+      <div className="panel answer" onClick={handleClick4}>
+        <p>{field4}</p>
         <div>
           <button>=</button>
         </div>
