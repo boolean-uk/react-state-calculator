d63cb6b63dd9629c722090edf96577becef2e045
msg <mats.gravem@no.experis.com>
2024-02-26 14:21:18 +0100

    Ext2

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,22 @@ import { useState } from "react"
 import "./App.css"
 
 const buttons = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-const operators = ['+', '-', '*', '÷']
+const operators = ['+', '-', '*', '/']
 
 function App() {
+  // UseState for each part of the calculator
   const [firstNumber, setFirstNumber] = useState('0')
   const [operator, setOperator] = useState('+')
   const [secondNumber, setSecondNumber] = useState('0')
   const [result, setResult] = useState('0')
+  const [storedValue, setStoredValue] = useState('0')
 
   return (
     <div className="calculator">
       <div className="panel">
         <p>{firstNumber}</p>
         <div className="numbers">
+          {/* Display all calculator face bottons and add the onclick functionality */}
           {buttons.map(string => 
             <button key={string} 
               onClick={() => {
@@ -22,6 +25,7 @@ function App() {
                 else {setFirstNumber(firstNumber + string)}
               }}>{string}</button>
           )}
+          <button onClick={() => setFirstNumber(storedValue)}>Stored</button>
           <button onClick={() => setFirstNumber('0')}>Clear</button>
         </div>
       </div>
@@ -45,13 +49,19 @@ function App() {
                 else {setSecondNumber(secondNumber + string)}
               }}>{string}</button>
           )}
+          <button onClick={() => setSecondNumber(storedValue)}>Stored</button>
           <button onClick={() => setSecondNumber('0')}>Clear</button>
         </div>
       </div>
       <div className="panel answer">
         <p>{result}</p>
         <div>
-          <button onClick={() => setResult(eval(firstNumber+operator+secondNumber))}>=</button>
+          <button onClick={() => {
+            /* Evaluate the equation string -> show two decimals if result is a float */
+            const answer = eval(firstNumber+operator+secondNumber)
+            setResult(Number.isInteger(answer) ? answer : answer.toFixed(2))
+          }}>=</button>
+          <button onClick={() => setStoredValue(result)}>Store Value</button>
         </div>
       </div>
     </div>
