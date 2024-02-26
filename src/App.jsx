6c46249ae5facd327c0fc7bf5f2d6da7dee05e3a
tonnesegeland <to.smith.eg@gmail.com>
2024-02-26 15:39:08 +0100

    Finished core, ext 1, ext 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,135 @@
+import { useState } from "react"
 import "./App.css"
 
+
+const buttonLabels = [
+  '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', 'Clear'
+]
+
+const signButtonLabels = [
+  '+', '-', '*', '÷'
+]
+
 function App() {
+  let [firstDec, setFirstDec] = useState(false)
+  let [firstDecimal, setFirstDecimal] = useState(1)
+
+  let [secondDec, setSecondDec] = useState(false)
+  let [secondDecimal, setSecondDecimal] = useState(1)
+  
+
+  const [first, setFirst] = useState(0)
+  const [sign, setSign] = useState('+')
+  const [second, setSecond] = useState(0)
+  const [result, setResult] = useState()
+
+  function addFirstNum(inp) {
+    if (inp === "Clear") {
+      setFirstDec(false)
+      setFirstDecimal(1)
+      setFirst(0)
+    }
+    else if (inp === ".") {
+      setFirstDec(true)
+    }
+    else {
+      let num = parseInt(inp)
+      if (!firstDec) {
+        setFirst(first*10+num)
+      }
+      else {
+        let d = parseFloat((num/(10**firstDecimal)).toFixed(firstDecimal))
+        setFirst(first+d)
+        setFirstDecimal(firstDecimal+1)
+      }
+    }
+  }
+
+  function addSecondNum(inp) {
+    if (inp === "Clear") {
+      setSecondDec(false)
+      setSecondDecimal(1)
+      setSecond(0)
+    }
+    else if (inp === ".") {
+      setSecondDec(true)
+    }
+    else {
+      let num = parseInt(inp)
+      if (!secondDec) {
+        setSecond(second*10+num)
+      }
+      else {
+        let d = parseFloat((num/(10**secondDecimal)).toFixed(secondDecimal))
+        setSecond(second+d)
+        setSecondDecimal(secondDecimal+1)
+      }
+    }
+  }
+
+  function calculate() {
+    let result = 0
+    switch (sign){
+      case '+': 
+        result = first+second
+        break
+      
+      case '-': 
+        result = first-second
+        break
+      
+      case '*': 
+        result = first*second
+        break
+      
+      case '÷': 
+        result = first/second
+        break
+    }
+    let r = 0
+    if (secondDecimal>firstDecimal){
+      r = secondDecimal - 1
+    }
+    else {
+      r = firstDecimal -1
+    }
+    setResult(result.toFixed(r))
+  
+  }
+
 
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
+        <p>{first}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          {buttonLabels.map(btn => 
+            <button onClick={() => addFirstNum(btn)}> {btn} </button>
+          )}
         </div>
       </div>
 
       <div className="panel">
-        <p>+</p>
+        <p>{sign}</p>
         <div className="numbers">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
+          {signButtonLabels.map(btn => 
+            <button onClick={() => setSign(btn)}> {btn} </button>
+          )}
         </div>
       </div>
 
       <div className="panel">
-        <p>0</p>
+        <p>{second}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+        {buttonLabels.map(btn => 
+            <button onClick={() => addSecondNum(btn)}> {btn} </button>
+          )}
         </div>
       </div>
       <div className="panel answer">
-        <p>0</p>
+        <p>{result}</p>
         <div>
-          <button>=</button>
+          <button onClick={calculate}>=</button>
         </div>
       </div>
     </div>
