1dc253fab9af3a355f7386ebcdcb635a074d5f43
ThomasKvam <thomas.e.h.kvam@hotmail.no>
2024-02-26 14:03:33 +0100

    Done with core and extension 1, attempting extension 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,111 @@
+import { useState } from "react"
 import "./App.css"
 
+let prevNumberOne = ""
+let prevNumberTwo = ""
+
+
 function App() {
+  const [numOne, setNumOne] = useState("0");
+  const [operator, setOperator] = useState("+")
+  const [numTwo, setNumTwo] = useState("0")
+  const [sum, setSum] = useState("0")
+  const [store, setStore] = useState("")
+
+  const displayNumberOne = (number) => {
+    if(prevNumberOne === ""){
+      setNumOne(number)
+      prevNumberOne += number
+      if(number === "0"){
+        prevNumberOne = ""
+      }
+    } else{
+      setNumOne(numOne + number)
+      prevNumberOne += number
+    }
+  }
+
+  const displayNumberTwo = (number) => {
+    if(prevNumberTwo === ""){
+      setNumTwo(number)
+      prevNumberTwo += number
+      if(number === "0"){
+        prevNumberTwo = ""
+      }
+    } else{
+      setNumTwo(numTwo + number)
+      prevNumberTwo += number
+    }
+  }
+
+  const displayOperator = (operator) => {
+    setOperator(operator)
+  }
+
+  const displaySum = () => {
+    if(operator === '+'){
+      setSum(parseInt(numOne) + parseInt(numTwo))
+    } else if(operator === '-'){
+      setSum(parseInt(numOne) - parseInt(numTwo))
+    } else if(operator === '÷'){
+      setSum(parseInt(numOne) / parseInt(numTwo))
+    } else if(operator === '*'){
+      setSum(parseInt(numOne) * parseInt(numTwo))
+    }
+  }
 
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
+        <p>{numOne}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={() => displayNumberOne("1")}>1</button>
+          <button onClick={() => displayNumberOne("2")}>2</button>
+          <button onClick={() => displayNumberOne("3")}>3</button>
+          <button onClick={() => displayNumberOne("4")}>4</button>
+          <button onClick={() => displayNumberOne("5")}>5</button>
+          <button onClick={() => displayNumberOne("6")}>6</button>
+          <button onClick={() => displayNumberOne("7")}>7</button>
+          <button onClick={() => displayNumberOne("8")}>8</button>
+          <button onClick={() => displayNumberOne("9")}>9</button>
+          <button onClick={() => displayNumberOne("0")}>0</button>
+          <button onClick={() => (setNumOne("0"), prevNumberOne= "")}>Clear</button>
+          <button onClick={() => (displayNumberOne(sum), prevNumberOne="")}>recall</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>+</p>
+        <p>{operator}</p>
         <div className="numbers">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
+          <button onClick={() => displayOperator('+')}>+</button>
+          <button onClick={() => displayOperator('-')}>-</button>
+          <button onClick={() => displayOperator('*')}>*</button>
+          <button onClick={() => displayOperator('÷')}>÷</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>0</p>
+        <p>{numTwo}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={() => displayNumberTwo("1")}>1</button>
+          <button onClick={() => displayNumberTwo("2")}>2</button>
+          <button onClick={() => displayNumberTwo("3")}>3</button>
+          <button onClick={() => displayNumberTwo("4")}>4</button>
+          <button onClick={() => displayNumberTwo("5")}>5</button>
+          <button onClick={() => displayNumberTwo("6")}>6</button>
+          <button onClick={() => displayNumberTwo("7")}>7</button>
+          <button onClick={() => displayNumberTwo("8")}>8</button>
+          <button onClick={() => displayNumberTwo("9")}>9</button>
+          <button onClick={() => displayNumberTwo("0")}>0</button>
+          <button onClick={() => (setNumTwo("0"), prevNumberTwo="")}>Clear</button>
+          <button onClick={() => displayNumberTwo(sum)}>recall</button>
         </div>
       </div>
       <div className="panel answer">
-        <p>0</p>
+        <p>{sum}</p>
         <div>
-          <button>=</button>
+          <button onClick={() => displaySum()}>=</button>
+          <button onClick={() => setStore(sum)}>store</button>
         </div>
       </div>
     </div>
