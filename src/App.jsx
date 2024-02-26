f949db5578143ca7e1c9f39a73861b9aaa65a5cf
stp <steventphung6@gmail.com>
2024-02-26 13:13:26 +0100

    Steven Tong Phung
    core complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,68 @@
 import "./App.css"
+import { useState } from 'react'
 
 function App() {
+  
+  // useStates
+  const [numberOne, setNumberOne] = useState('0')
+  const [operator, setOperator] = useState('+')
+  const [numberTwo, setNumberTwo] = useState('0')
+  const [answer, setAnswer] = useState('0')
+
+  const handleClick1 = (event) => {
+    // Ensure the clicked element is a button before proceeding.
+    if (event.target.tagName === 'BUTTON') {
+      if (event.target.innerText === 'Clear'){
+        setNumberOne('0')
+      }
+      else{
+        setNumberOne(event.target.innerText)
+      }
+    }
+  }
+  const handleClick2 = (event) => {
+    // Ensure the clicked element is a button before proceeding.
+    if (event.target.tagName === 'BUTTON') {
+        setOperator(event.target.innerText)
+      }
+  }
+  
+  const handleClick3 = (event) => {
+    // Ensure the clicked element is a button before proceeding.
+    if (event.target.tagName === 'BUTTON') {
+      if (event.target.innerText === 'Clear'){
+        setNumberTwo('0')
+      }
+      else{
+        setNumberTwo(event.target.innerText)
+      }
+    }
+  }
+
+  const handleClick4 = (event) => {
+    // Ensure the clicked element is a button before proceeding.
+    if (event.target.tagName === 'BUTTON') {
+      if(operator === '-') {
+        setAnswer(Number(numberOne) - Number(numberTwo))
+      }
+      else if(operator === '+') {
+        setAnswer(Number(numberOne) + Number(numberTwo))
+      }
+      else if(operator === '*') {
+        setAnswer(Number(numberOne) * Number(numberTwo))
+      }
+      else {
+        setAnswer((Number(numberOne) / Number(numberTwo)).toFixed(5))
+      }
+    }
+  }
+
 
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
-        <div className="numbers">
+        <p>{numberOne}</p>
+        <div className="numbers" onClick = {handleClick1}>
           <button>1</button>
           <button>2</button>
           <button>3</button>
@@ -22,8 +78,8 @@ function App() {
       </div>
 
       <div className="panel">
-        <p>+</p>
-        <div className="numbers">
+        <p>{operator}</p>
+        <div className="numbers" onClick = {handleClick2}>
           <button>+</button>
           <button>-</button>
           <button>*</button>
@@ -32,8 +88,8 @@ function App() {
       </div>
 
       <div className="panel">
-        <p>0</p>
-        <div className="numbers">
+        <p>{numberTwo}</p>
+        <div className="numbers" onClick = {handleClick3}>
           <button>1</button>
           <button>2</button>
           <button>3</button>
@@ -47,8 +103,8 @@ function App() {
           <button>Clear</button>
         </div>
       </div>
-      <div className="panel answer">
-        <p>0</p>
+      <div className="panel answer" onClick = {handleClick4}>
+        <p>{answer}</p>
         <div>
           <button>=</button>
         </div>
