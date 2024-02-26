d85498e05b559829b4c656e48314b4b17215bb63
Mikolaj <mkmbaran@gmail.com>
2024-02-26 09:05:05 +0100

    Core complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,85 @@
 import "./App.css"
+import {useState} from 'react'
+
 
 function App() {
+  const [firstNum, setFirstNum] = useState("0")
+  const [secondNum, setSecondNum] = useState("0")
+  const [operator, setOperator] = useState("+")
+  const [result, setResult] = useState("0")
+
+  //const values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+  //const operators = ["+", "-", "*", "/"]
+
+  const Calculate = () => {
+    let a = parseFloat(firstNum)
+    let b = parseFloat(secondNum)
+  
+    switch(operator){
+      case "+":
+          setResult(a + b);
+          break;
+      case "-":
+          setResult(a - b);
+          break;
+      case "*":
+          setResult(a * b);
+          break;
+      case "÷":
+          setResult(a / b);
+          break;
+    }
+  }
 
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
+        <p>{firstNum}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={() => setFirstNum(1)}>1</button>
+          <button onClick={() => setFirstNum(2)}>2</button>
+          <button onClick={() => setFirstNum(3)}>3</button>
+          <button onClick={() => setFirstNum(4)}>4</button>
+          <button onClick={() => setFirstNum(5)}>5</button>
+          <button onClick={() => setFirstNum(6)}>6</button>
+          <button onClick={() => setFirstNum(7)}>7</button>
+          <button onClick={() => setFirstNum(8)}>8</button>
+          <button onClick={() => setFirstNum(9)}>9</button>
+          <button onClick={() => setFirstNum(0)}>0</button>
+        </div>
+        <button onClick={()=>{setFirstNum('0')}}>Clear</button>
+      </div>
+      <div className="panel">
+        <p>{operator}</p>
+        <div className="numbers">
+          <button onClick={() => setOperator('+')}>+</button>
+          <button onClick={() => setOperator('-')}>-</button>
+          <button onClick={() => setOperator('*')}>*</button>
+          <button onClick={() => setOperator('÷')}>÷</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>+</p>
+        <p>{secondNum}</p>
         <div className="numbers">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
-        </div>
-      </div>
-
-      <div className="panel">
-        <p>0</p>
-        <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={() => setSecondNum(1)}>1</button>
+          <button onClick={() => setSecondNum(2)}>2</button>
+          <button onClick={() => setSecondNum(3)}>3</button>
+          <button onClick={() => setSecondNum(4)}>4</button>
+          <button onClick={() => setSecondNum(5)}>5</button>
+          <button onClick={() => setSecondNum(6)}>6</button>
+          <button onClick={() => setSecondNum(7)}>7</button>
+          <button onClick={() => setSecondNum(8)}>8</button>
+          <button onClick={() => setSecondNum(9)}>9</button>
+          <button onClick={() => setSecondNum(0)}>0</button>
+        <button onClick={() => {setSecondNum('0')}}>Clear</button>
         </div>
       </div>
       <div className="panel answer">
-        <p>0</p>
-        <div>
-          <button>=</button>
-        </div>
+        <p>{result}</p>
+        <button onClick={()=>{Calculate()}}>=</button>
       </div>
     </div>
   )
 }
-
 export default App
