c7c54a9a7571f639408d6c4c51e2f50752414778
Nicolai Klokmose <angryniczorn@gmail.com>
2024-02-26 13:13:00 +0100

    initial commit, core done

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,100 @@
-import "./App.css"
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [firstDigits, setFirstDigits] = useState(0);
+  const [operators, setOperators] = useState(null);
+  const [secondDigits, setSecondDigits] = useState(0);
+  const [results, setResults] = useState(0);
+
+  const updateFirstDigit = (digit) => {
+    setFirstDigits(digit);
+  };
+
+  const updateSecondDigit = (digit) => {
+    setSecondDigits(digit);
+  };
+
+  const updateOperator = (operator) => {
+    setOperators(operator);
+  };
+
+  const resetFirstDigit = () => {
+    setFirstDigits(0);
+  };
+
+  const resetSecondDigit = () => {
+    setSecondDigits(0);
+  };
+
+  const updateResult = () => {
+    console.log(operators)
+    switch (operators) {
+      case "+":
+        console.log("its a plus");
+        setResults(firstDigits + secondDigits);
+        break;
+      case "-":
+        console.log("its a minus");
+        setResults(firstDigits - secondDigits);
+        break;
+      case "*":
+        console.log("its a multiplication");
+        setResults(firstDigits * secondDigits);
+        break;
+      case "÷":
+        console.log("its a division");
+        setResults(firstDigits / secondDigits);
+        break;
+      default:
+        setResults(null);
+    }
+  };
 
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
+        <p>{firstDigits}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((digit) => (
+            <button key={digit} onClick={() => updateFirstDigit(digit)}>
+              {digit}
+            </button>
+          ))}
+          <button onClick={resetFirstDigit}>Clear</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>+</p>
+        <p>{operators}</p>
         <div className="numbers">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
+          {["+", "-", "*", "÷"].map((operator) => (
+            <button key={operator} onClick={() => updateOperator(operator)}>
+              {operator}
+            </button>
+          ))}
         </div>
       </div>
 
       <div className="panel">
-        <p>0</p>
+        <p>{secondDigits}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((digit) => (
+            <button key={digit} onClick={() => updateSecondDigit(digit)}>
+              {digit}
+            </button>
+          ))}
+          <button onClick={resetSecondDigit}>Clear</button>
         </div>
       </div>
       <div className="panel answer">
-        <p>0</p>
+        <p>{results}</p>
         <div>
-          <button>=</button>
+          <button onClick={updateResult}>=</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
