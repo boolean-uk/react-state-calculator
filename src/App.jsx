d312f6bd4498848a155a10e45c6b66b7492ba5a4
Knut S Rostad <knut.rostad@no.experis.com>
2024-02-26 13:32:26 +0100

    Extension 1 and 2 done

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,23 @@ function App() {
   const [field2, setField2] = useState('+')
   const [field3, setField3] = useState(0)
   const [field4, setField4] = useState(0)
+  const [stored, setStored] = useState(0)
 
   const handleClick1 = (event) =>{
     if (event.target.tagName === 'BUTTON') {
+      //Parse the field so we can check if it is a zero
+      const parsed1 = parseFloat(field1)
       if(event.target.innerText === 'Clear') {
         setField1(0)
-      }else{
+      }else if(event.target.innerText === 'Use stored'){
+        setField1(parseFloat(stored))
+      }else if(parsed1 === 0){
+        //If it is 0 set it to a new number
         setField1(event.target.innerText)
-      }
+      }else if(parsed1 !== 0){
+        //If it is not 0, append the number
+        setField1(parsed1+event.target.innerText)
+      } 
       
     }
   }
@@ -26,31 +35,43 @@ function App() {
 
   const handleClick3 = (event) =>{
     if (event.target.tagName === 'BUTTON') {
+      const parsed3 = parseFloat(field3)
       if(event.target.innerText === 'Clear') {
         setField3(0)
-      }else{
+      }else if(event.target.innerText === 'Use stored'){
+        setField3(parseFloat(stored))
+      }else if(parsed3 === 0){
+        //If it is 0 set it to a new number
         setField3(event.target.innerText)
-      }
-      
+      }else if(parsed3 !== 0){
+        //If it is not 0, append the number
+        setField3(parsed3+event.target.innerText)
+      } 
     }
   }
 
   const handleClick4 = (event) =>{
     if (event.target.tagName === 'BUTTON') {
-      let answer = 0
-      const parsed1 = parseFloat(field1)
-      const parsed3 = parseFloat(field3)
-      if(field2 === '+'){
-        answer = parsed1+parsed3
-      }else if(field2 === '-'){
-        answer = parsed1-parsed3
-      }else if(field2 === '÷'){
-        answer = parsed1/parsed3
-      }else if(field2 === '*'){
-        answer = parsed1*parsed3
+      //Handle answer
+      if(event.target.innerText === '=') {
+        let answer = 0
+        const parsed1 = parseFloat(field1)
+        const parsed3 = parseFloat(field3)
+        if(field2 === '+'){
+          answer = parsed1+parsed3
+        }else if(field2 === '-'){
+          answer = parsed1-parsed3
+        }else if(field2 === '÷'){
+          answer = parsed1/parsed3
+        }else if(field2 === '*'){
+          answer = parsed1*parsed3
+        }
+        setField4(answer)
+
+      } else if(event.target.innerText === 'Store'){
+        setStored(field4)
       }
-      setField4(answer)
-    }
+  }
   }
 
 
@@ -70,6 +91,8 @@ function App() {
           <button>9</button>
           <button>0</button>
           <button>Clear</button>
+          <button>Use stored</button>
+          <button>,</button>
         </div>
       </div>
 
@@ -97,12 +120,16 @@ function App() {
           <button>9</button>
           <button>0</button>
           <button>Clear</button>
+          <button>Use stored</button>
+          <button>,</button>
         </div>
       </div>
       <div className="panel answer" onClick={handleClick4}>
         <p>{field4}</p>
         <div>
           <button>=</button>
+          <button>Store</button>
+          <p>Stored answer: {stored}</p>
         </div>
       </div>
     </div>
