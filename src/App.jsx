0aa2e83e2df249fba50fa36e3d08083f5864dcfd
Knut S Rostad <knut.rostad@no.experis.com>
2024-02-26 15:03:21 +0100

    extension 3 done

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,19 @@ function App() {
         setField1(0)
       }else if(event.target.innerText === 'Use stored'){
         setField1(parseFloat(stored))
-      }else if(parsed1 === 0){
+      }else if(event.target.innerText === ','){
+        //Check if it is not already a decimal
+        if(!field1.toString().includes('\.')){
+          setField1(parseFloat(field1)+'.')
+        }
+      }else if(parsed1 === 0 && !field1.toString().includes('\.')){
         //If it is 0 set it to a new number
         setField1(event.target.innerText)
-      }else if(parsed1 !== 0){
+      }else if(parsed1 !== 0 || field1 === '0.'){
         //If it is not 0, append the number
-        setField1(parsed1+event.target.innerText)
+        const extra = event.target.innerText
+        setField1(field1 + extra)
       } 
-      
     }
   }
 
@@ -40,12 +45,18 @@ function App() {
         setField3(0)
       }else if(event.target.innerText === 'Use stored'){
         setField3(parseFloat(stored))
-      }else if(parsed3 === 0){
+      }else if(event.target.innerText === ','){
+        //Check if it is not already a decimal
+        if(!field3.toString().includes('\.')){
+          setField3(parseFloat(field3)+'.')
+        }
+      }else if(parsed3 === 0 && !field3.toString().includes('\.')){
         //If it is 0 set it to a new number
         setField3(event.target.innerText)
-      }else if(parsed3 !== 0){
+      }else if(parsed3 !== 0 || field3 === '0.'){
         //If it is not 0, append the number
-        setField3(parsed3+event.target.innerText)
+        const extra = event.target.innerText
+        setField3(field3 + extra)
       } 
     }
   }
@@ -66,8 +77,12 @@ function App() {
         }else if(field2 === '*'){
           answer = parsed1*parsed3
         }
-        setField4(answer)
-
+        //Restrict the return number to 2 decimals or without decimals if it is an int
+        if(Number.isInteger(answer)){
+          setField4(answer)
+        }else{
+          setField4(parseFloat(answer).toFixed(2))
+        }
       } else if(event.target.innerText === 'Store'){
         setStored(field4)
       }
