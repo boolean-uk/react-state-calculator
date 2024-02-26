cc2ca4caf2724ffe35d8805fc39b48dd8f16ed8a
hanna <hannaphamm1007@gmail.com>
2024-02-26 13:23:39 +0100

    core

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,98 @@
-import "./App.css"
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [numberPanelOne, setNumberPanelOne] = useState(0);
+  const [numberPanelTwo, setNumberPanelTwo] = useState(0);
+  const [operation, setOperation] = useState("+");
+  const [result, setResult] = useState(0);
+
+  const displayNumberPanelOne = (num) => {
+    setNumberPanelOne(num);
+  };
+
+  const displayNumberPanelTwo = (num) => {
+    setNumberPanelTwo(num);
+  };
+
+  const displayOperation = (op) => {
+    setOperation(op);
+  };
+
+  const calculateResult = () => {
+    switch (operation) {
+      case "+":
+        setResult(numberPanelOne + numberPanelTwo);
+        break;
+      case "-":
+        setResult(numberPanelOne - numberPanelTwo);
+        break;
+      case "*":
+        setResult(numberPanelOne * numberPanelTwo);
+        break;
+      case "÷":
+        setResult(numberPanelOne / numberPanelTwo);
+        break;
+      default:
+        setResult(0);
+        break;
+    }
+  };
 
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
+        <p>{numberPanelOne}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={() => displayNumberPanelOne(1)}>1</button>
+          <button onClick={() => displayNumberPanelOne(2)}>2</button>
+          <button onClick={() => displayNumberPanelOne(3)}>3</button>
+          <button onClick={() => displayNumberPanelOne(4)}>4</button>
+          <button onClick={() => displayNumberPanelOne(5)}>5</button>
+          <button onClick={() => displayNumberPanelOne(6)}>6</button>
+          <button onClick={() => displayNumberPanelOne(7)}>7</button>
+          <button onClick={() => displayNumberPanelOne(8)}>8</button>
+          <button onClick={() => displayNumberPanelOne(9)}>9</button>
+          <button onClick={() => displayNumberPanelOne(0)}>0</button>
+          <button onClick={() => setNumberPanelOne(0)}>Clear</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>+</p>
+        <p>{operation}</p>
         <div className="numbers">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
+          <button onClick={() => displayOperation('+')}>+</button>
+          <button onClick={() => displayOperation('-')}>-</button>
+          <button onClick={() => displayOperation('*')}>*</button>
+          <button onClick={() => displayOperation('÷')}>÷</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>0</p>
+        <p>{numberPanelTwo}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={() => displayNumberPanelTwo(1)}>1</button>
+          <button onClick={() => displayNumberPanelTwo(2)}>2</button>
+          <button onClick={() => displayNumberPanelTwo(3)}>3</button>
+          <button onClick={() => displayNumberPanelTwo(4)}>4</button>
+          <button onClick={() => displayNumberPanelTwo(5)}>5</button>
+          <button onClick={() => displayNumberPanelTwo(6)}>6</button>
+          <button onClick={() => displayNumberPanelTwo(7)}>7</button>
+          <button onClick={() => displayNumberPanelTwo(8)}>8</button>
+          <button onClick={() => displayNumberPanelTwo(9)}>9</button>
+          <button onClick={() => displayNumberPanelTwo(0)}>0</button>
+          <button onClick={() => setNumberPanelTwo(0)}>Clear</button>
         </div>
       </div>
+
       <div className="panel answer">
-        <p>0</p>
+        <p>{result}</p>
         <div>
-          <button>=</button>
+          <button onClick={calculateResult}>=</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
