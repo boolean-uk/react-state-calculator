fd84f38dc6201f1f06dfb88845e23125283613d1
guro18 <rollgustav95@gmail.com>
2024-03-05 20:30:35 +0100

    updated based on feedback

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import "./App.css"
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 function App() {
-  const [firstPanelNum, updateNum1] = useState(0);
-  const [secondPanelNum, updateNum2] = useState(0);
+  const [firstPanelNum, updateFirstNum] = useState(0);
+  const [secondPanelNum, updateSecondNum] = useState(0);
   const [operator, setOperator] = useState('+');
   const [result, setResult] = useState(0);
 
   const handleClickPanel1 = (num) => {
-    updateNum1(num);
+    updateFirstNum(num);
   };
 
   const handleClickPanel2 = (num) => {
-    updateNum2(num)
+    updateSecondNum(num)
   };
 
-  const handleClickOperator = (operator) => {
+  const updateOperator = (operator) => {
     setOperator(operator)
   };
 
-  const handleEquals = () => {
+  const computeTotal = () => {
     let res;
     switch (operator) {
       case '+':
@@ -45,50 +45,38 @@ function App() {
       <div className="panel">
         <p>{firstPanelNum}</p>
         <div className="numbers">
-          <button onClick={() => handleClickPanel1(1)}>1</button>
-          <button onClick={() => handleClickPanel1(2)}>2</button>
-          <button onClick={() => handleClickPanel1(3)}>3</button>
-          <button onClick={() => handleClickPanel1(4)}>4</button>
-          <button onClick={() => handleClickPanel1(5)}>5</button>
-          <button onClick={() => handleClickPanel1(6)}>6</button>
-          <button onClick={() => handleClickPanel1(7)}>7</button>
-          <button onClick={() => handleClickPanel1(8)}>8</button>
-          <button onClick={() => handleClickPanel1(9)}>9</button>
-          <button onClick={() => handleClickPanel1(0)}>0</button>
-          <button onClick={() => handleClickPanel1(0)}>Clear</button>
+          {[...Array(10)].map((_, index) => (
+            <button key={index} onClick={() => handleClickPanel1(index)}>
+              {index === 9 ? "Clear" : index}
+            </button>
+          ))}
         </div>
       </div>
 
       <div className="panel">
         <p>{operator}</p>
         <div className="numbers">
-          <button onClick={() => handleClickOperator('+')}>+</button>
-          <button onClick={() => handleClickOperator('-')}>-</button>
-          <button onClick={() => handleClickOperator('*')}>*</button>
-          <button onClick={() => handleClickOperator('÷')}>÷</button>
+          <button onClick={() => updateOperator('+')}>+</button>
+          <button onClick={() => updateOperator('-')}>-</button>
+          <button onClick={() => updateOperator('*')}>*</button>
+          <button onClick={() => updateOperator('÷')}>÷</button>
         </div>
       </div>
 
       <div className="panel">
         <p>{secondPanelNum}</p>
         <div className="numbers">
-          <button onClick={() => handleClickPanel2(1)}>1</button>
-          <button onClick={() => handleClickPanel2(2)}>2</button>
-          <button onClick={() => handleClickPanel2(3)}>3</button>
-          <button onClick={() => handleClickPanel2(4)}>4</button>
-          <button onClick={() => handleClickPanel2(5)}>5</button>
-          <button onClick={() => handleClickPanel2(6)}>6</button>
-          <button onClick={() => handleClickPanel2(7)}>7</button>
-          <button onClick={() => handleClickPanel2(8)}>8</button>
-          <button onClick={() => handleClickPanel2(9)}>9</button>
-          <button onClick={() => handleClickPanel2(0)}>0</button>
-          <button onClick={() => handleClickPanel2(0)}>Clear</button>
+          {[...Array(10)].map((_, index) => (
+              <button key={index} onClick={() => handleClickPanel2(index)}>
+                {index === 9 ? "Clear" : index}
+              </button>
+            ))}
         </div>
       </div>
       <div className="panel answer">
         <p>{result}</p>
         <div>
-          <button onClick={() => handleEquals()}>=</button>
+          <button onClick={() => computeTotal()}>=</button>
         </div>
       </div>
     </div>
